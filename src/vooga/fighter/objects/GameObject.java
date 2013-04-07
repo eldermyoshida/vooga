a562b912ebca02f81d7caa17438817e3a16cdc2d
th365thli <th365thli@gmail.com>
2013-04-07 17:58:04 -0400

    game model initial

---
+++ src/vooga/fighter/objects/GameObject.java
@@ -0,0 +1,16 @@
+package vooga.fighter.objects;
+
+import java.awt.Graphics2D;
+
+public abstract class GameObject {
+    
+    public void paint(Graphics2D pen) {
+        
+
+    }
+    
+    public void update(double stepTime) {
+        
+    }
+
+}
