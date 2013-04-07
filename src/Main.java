13584da7858f4f6f8d52300cb1fee176fa9bee26
Robert C. Duvall <robertcduvall@gmail.com>
2013-04-07 10:42:58 -0400

    Added main for the project

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * The one class that starts the entire project.
+ * 
+ * @author ADD YOUR NAME HERE
+ */
+public class Main {
+    private Main () {
+        // does not make sense to construct this class
+    }
+
+    /**
+     * main --- where the program starts
+     * 
+     * @param args anything passed in from the command-line
+     */
+    public static void main (String[] args) {
+        // TODO add code here to start the Arcade
+    }
+}
