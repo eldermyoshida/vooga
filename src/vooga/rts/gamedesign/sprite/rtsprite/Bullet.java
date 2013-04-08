f8baa60da446a72e4b78901f842a0fe54af291d9
Ryan Fishel <fishel.ryan@gmail.com>
2013-04-08 05:47:59 -0400

    modified bullet

--- src/vooga/rts/gamedesign/sprite/rtsprite/Bullet.java
+++ src/vooga/rts/gamedesign/sprite/rtsprite/Bullet.java
@@ -13,22 +13,5 @@ public class Bullet extends Projectile{
 			int damage, int health) {
 		super(pixmap, loc, size, sound, damage, health);
 	}
-
-	@Override
-	public int calculateDamage() {
-		return 0;
-	}
-
-	@Override
-	public void changeHealth() {
-		return;
-		
-	}
-
-	@Override
-	public void update(double elapsedTime) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
+
