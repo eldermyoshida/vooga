5d2fe5579e5b7bdeae7afd1e2859ebf7128db5cb
ej48 <ej48@duke.edu>
2013-04-08 15:22:55 -0400

    initial try for pictures in button panel

--- src/arcade/view/panels/ButtonPanel.java
+++ src/arcade/view/panels/ButtonPanel.java
@@ -2,7 +2,10 @@ package arcade.view.panels;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import arcade.view.MainView;
 
@@ -13,8 +16,8 @@ public class ButtonPanel extends JPanel {
      */
     private static final long serialVersionUID = 7307619822247104115L;
     private static final int WINDOW_HEIGHT = 600;
-    private static final int BUTTON_WIDTH = 120;
-    private static final int BUTTON_HEIGHT = 200;
+    private static final int BUTTON_WIDTH = 150;
+    private static final int BUTTON_HEIGHT = 150;
     private static final int ORIGIN = 0;
     private static final int GAMECENTER_INDEX = 0;
     private static final int SOCIALCENTER_INDEX = 1;
@@ -30,18 +33,31 @@ public class ButtonPanel extends JPanel {
      */
     public ButtonPanel (MainView upperLevel) {
         myUpperLevelPanel = upperLevel;
-        setLayout(null);
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         // Buttons
-        JButton gameCenterButton = new JButton("Game Center");
-        JButton socialCenterButton = new JButton("Social Center");
-        JButton storeButton = new JButton("Store");
+        String localDirectory = System.getProperty("user.dir");
+        ImageIcon gameCenterIcon = new ImageIcon(localDirectory+"/src/arcade/resources/images/GameCenterIcon.jpg");
+        ImageIcon socialCenterIcon = new ImageIcon(localDirectory+"/src/arcade/resources/images/SocialCenterIcon.jpg");
+        ImageIcon storeIcon = new ImageIcon(localDirectory+"/src/arcade/resources/images/StoreIcon.jpg");
+        JButton gameCenterButton = new JButton(gameCenterIcon);
+        JButton socialCenterButton = new JButton(socialCenterIcon);
+        JButton storeButton = new JButton(storeIcon);
         gameCenterButton.setBounds(ORIGIN, ORIGIN, BUTTON_WIDTH, BUTTON_HEIGHT);
         socialCenterButton.setBounds(ORIGIN, WINDOW_HEIGHT / 2 - BUTTON_HEIGHT / 2, BUTTON_WIDTH,
                                      BUTTON_HEIGHT);
         storeButton.setBounds(ORIGIN, WINDOW_HEIGHT - BUTTON_HEIGHT, BUTTON_WIDTH, BUTTON_HEIGHT);
+        
+        JLabel gameCenterLabel = new JLabel("<html><b><font size = 5><font face = champion>Game Center</font></font></b></html>");
+        JLabel socialCenterLabel = new JLabel("<html><b><font size = 5><font face = champion>Social Center</font></font></b></html>");
+        JLabel storeLabel = new JLabel("<html><b><font size = 5><font face = champion>Social</font></font></b></html>");
+        
+        
         add(gameCenterButton);
+        add(gameCenterLabel);
         add(socialCenterButton);
+        add(socialCenterLabel);
         add(storeButton);
+        add(storeLabel);
 
         // Set-up Action Listener
         gameCenterButton.addActionListener(actionListenerConstructor(GAMECENTER_INDEX));
@@ -55,7 +71,6 @@ public class ButtonPanel extends JPanel {
             @Override
             public void actionPerformed (ActionEvent e) {
                 myUpperLevelPanel.changeViewPanel(index);
-                System.out.println("pushed " + index + " to MainView");
             }
         };
         return output;
