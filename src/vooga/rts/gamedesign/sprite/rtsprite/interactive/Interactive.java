784ff35c47a6ad5fa43830a308a65a7721df634e
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-04-08 00:48:54 -0400

    Formatted

--- src/vooga/rts/gamedesign/sprite/rtsprite/interactive/Interactive.java
+++ src/vooga/rts/gamedesign/sprite/rtsprite/interactive/Interactive.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import vooga.rts.gamedesign.sprite.rtsprite.IAttackable;
 import vooga.rts.gamedesign.sprite.rtsprite.IGatherable;
 import vooga.rts.gamedesign.sprite.rtsprite.RTSprite;
@@ -26,26 +25,25 @@ import vooga.rts.util.Pixmap;
 import vooga.rts.util.Sound;
 import vooga.rts.gamedesign.factories.Factory;
 
+
 /**
  * 
  * @author Ryan Fishel
  * @author Kevin Oh
  * @author Francesco Agosti
- * @author Wenshun Liu 
- *
+ * @author Wenshun Liu
+ * 
  */
 
 public abstract class Interactive extends RTSprite implements RTSpriteVisitor {
 
-
-    /** 
-     *  the data structure for storing progress of upgrades can be changed? 
+    /**
+     * the data structure for storing progress of upgrades can be changed?
      */
     private AttackStrategy myAttackStrategy;
 
     private GatherStrategy myGatherStrategy;
     private OccupyStrategy myOccupyStrategy;
-
 
     private UpgradeTree myUpgradeTree;
 
@@ -54,67 +52,74 @@ public abstract class Interactive extends RTSprite implements RTSpriteVisitor {
     private SkillStrategy mySkillStrategy;
 
     private Map<String, Factory> myMakers;
-    
+
     private int myVision;
 
-    public Interactive (Pixmap image, Location center, Dimension size, Sound sound, int teamID, int health) {
+    public Interactive (Pixmap image,
+                        Location center,
+                        Dimension size,
+                        Sound sound,
+                        int teamID,
+                        int health) {
         super(image, center, size, sound, teamID, health);
         myMakers = new HashMap<String, Factory>();
         myAttackStrategy = new CannotAttack();
         myGatherStrategy = new CannotGather();
         myOccupyStrategy = new CannotOccupy();
-        
+
     }
-    
-    
-        /**
-         * Sets the attack strategy for an interactive. Can set the interactive
-         * to CanAttack or to CannotAttack and then can specify how it would
-         * attack. 
-         * @param newStrategy is the new attack strategy that the interactive
-         * will have
-         */
-    public void setAttackStrategy(AttackStrategy newStrategy){
-    	myAttackStrategy = newStrategy;
+
+    /**
+     * Sets the attack strategy for an interactive. Can set the interactive
+     * to CanAttack or to CannotAttack and then can specify how it would
+     * attack.
+     * 
+     * @param newStrategy is the new attack strategy that the interactive
+     *        will have
+     */
+    public void setAttackStrategy (AttackStrategy newStrategy) {
+        myAttackStrategy = newStrategy;
     }
-    
+
     /**
      * Sets the gatehr strategy for an interactive. Can set the interactive
      * to CanGather or to CannotGather and then can specify how it would
-     * gather. 
+     * gather.
+     * 
      * @param newStrategy is the new gather strategy that the interactive
-     * will have
+     *        will have
      */
-    public void setGatherStrategy(GatherStrategy newStrategy){
-    	myGatherStrategy = newStrategy;
-    }
-    
-    /**
-     * Sets the occupy strategy for an interactive. Can set the interactive
-     * to CanOccupy or to CannotOccupy. 
-     * @param newStrategy is the new occupy strategy that the interactive
-     * will have
-     */
-    public void setOccupyStrategy(OccupyStrategy newStrategy){
-    	myOccupyStrategy = newStrategy;
-    }
-    /**
-     * Returns the current attack strategy of the interactive
-     * @return the current attack strategy
-     */
-    public AttackStrategy getAttackstrategy(){
-        return myAttackStrategy;
-    }
-    
-    /**
-     * upgrades the interactive based on the selected upgrade
-     * @param upgrade is the upgrade that the interactive will get
-     */
-    public void upgrade(Upgrade upgrade) {
-    	upgrade.apply(this);
+    public void setGatherStrategy (GatherStrategy newStrategy) {
+        myGatherStrategy = newStrategy;
     }
 
-    
-    
+    /**
+     * Sets the occupy strategy for an interactive. Can set the interactive
+     * to CanOccupy or to CannotOccupy.
+     * 
+     * @param newStrategy is the new occupy strategy that the interactive
+     *        will have
+     */
+    public void setOccupyStrategy (OccupyStrategy newStrategy) {
+        myOccupyStrategy = newStrategy;
+    }
+
+    /**
+     * Returns the current attack strategy of the interactive
+     * 
+     * @return the current attack strategy
+     */
+    public AttackStrategy getAttackstrategy () {
+        return myAttackStrategy;
+    }
+
+    /**
+     * upgrades the interactive based on the selected upgrade
+     * 
+     * @param upgrade is the upgrade that the interactive will get
+     */
+    public void upgrade (Upgrade upgrade) {
+        upgrade.apply(this);
+    }
 
 }
