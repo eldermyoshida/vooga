875b82d13c1aa4384f544aec54bbdf5525cdb1f1
Jimmy Longley <jimmylongley@gmail.com>
2013-04-09 18:59:45 -0400

    changes to GameMap constructor

---
+++ src/vooga/towerdefense/model/Tile.java
@@ -0,0 +1,52 @@
+package vooga.towerdefense.model;
+
+import vooga.towerdefense.gameElements.Tower;
+import vooga.towerdefense.util.Location;
+
+
+public class Tile {
+    private boolean myIsWalkable;
+    private boolean myIsBuildable;
+    private Location myCenter;
+    private Tower myTower;
+    
+	public Tile(Location center, boolean walkable, boolean buildable) {
+		myIsWalkable = walkable;
+		myIsBuildable = buildable;
+		myCenter = center;
+	}
+
+    public void update(double elapsedTime) {
+        if (myTower != null) {
+            myTower.update(elapsedTime);
+        }        
+    }
+    
+    public void setWalkable(boolean walkable) {
+        myIsWalkable = walkable;
+    }
+    
+    public boolean isWalkable() {
+        return myIsWalkable;
+    }
+    
+    public void setBuildable(boolean buildable) {
+        myIsBuildable = buildable;
+    }
+    
+    public boolean isBuildable() {
+        return myIsBuildable;
+    }
+    
+    public void setTower(Tower t) {
+        myTower = t;
+        setBuildable(false);
+        setWalkable(false);
+    }
+    
+    public void deleteTower() {
+        myTower = null;
+        setBuildable(true);
+    }
+     
+}
