96e50c2eb61e54d701c302a88dc5e29637b4cf04
Wayne You <waynexyou@gmail.com>
2013-04-10 14:26:17 -0400

    Added in a way of annotating variables to be displayed as HUD Elements.

---
+++ src/vooga/fighter/util/HUDVariable.java
@@ -0,0 +1,9 @@
+package vooga.fighter.util;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface HUDVariable {
+    public String name();
+    public String HUDElementClass();
+}
