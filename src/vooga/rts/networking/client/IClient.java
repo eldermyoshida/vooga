a5959ac0e3ca043982f146db88ca21f6ac6b027c
srwareham <srwareham@hotmail.com>
2013-04-11 23:41:27 -0400

    IClient Description, removed now pointless old interface

--- src/vooga/rts/networking/client/IClient.java
+++ src/vooga/rts/networking/client/IClient.java
@@ -2,7 +2,13 @@ package vooga.rts.networking.client;
 
 import vooga.rts.networking.communications.Message;
 
-
+/**
+ * Interface for game to be able to access features of the client / server setup.
+ * Presumably this will be implemented in a thread that will continuously send and receive
+ * messages to the server
+ * @author srwareham
+ *
+ */
 public interface IClient {
     public abstract void sendData (Message message);
 
