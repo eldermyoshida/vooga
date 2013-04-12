0a79cda52ce8d61bf4ee8eec02ebcd4f613c6ae2
Scott Valentine <scott.douglas.valentine@gmail.com>
2013-04-12 15:51:50 -0400

    changed pixmap and animation to implement IPaintable which is painted by sprite

---
+++ src/vooga/scroller/sprites/IPaintable.java
@@ -0,0 +1,12 @@
+package vooga.scroller.sprites;
+
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+public interface IPaintable {
+
+    
+    public void paint (Graphics2D pen, Point2D center, Dimension size, double angle);
+    
+}
