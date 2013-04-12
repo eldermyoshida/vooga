f4e4c01d077dcbd1eee4052d3469ee8036060c0b
Erick Gonzalez <erick@erick-K52Jc.(none)>
2013-04-11 22:43:58 -0400

    Back to point

--- src/vooga/towerdefense/controller/Controller.java
+++ src/vooga/towerdefense/controller/Controller.java
@@ -3,15 +3,25 @@ package vooga.towerdefense.controller;
 import vooga.towerdefense.util.Location;
 
 public class Controller {
-
+    private boolean onBuildMode;
+    
+    
+    public Controller() {
+        onBuildMode = false;
+    }
     
     /**
      * handles a click to the map appropriately depending
      *          on the mode.
      * @param p is the location of the click
      */
-    public void handleMapClick(Location p) {
-        //TODO: handle a map click
+    public void handleMapClick(Point p) {
+        if (onBuildMode) {
+            
+        } 
+        else {
+            
+        }
     }
     
     /**
