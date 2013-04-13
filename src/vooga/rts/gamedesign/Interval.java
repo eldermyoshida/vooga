e5bb00f0a05c20b5281c78419a8abd83f2f6ea82
Ryan Fishel <fishel.ryan@gmail.com>
2013-04-13 14:00:47 -0400

    added interval class to handle the cooldowns of weapons and creating entities

---
+++ src/vooga/rts/gamedesign/Interval.java
@@ -0,0 +1,63 @@
+package vooga.rts.gamedesign;
+
+/**
+ * This class deals with cooldowns.  For example, after a weapon shoots, it 
+ * needs to have a delay before it can shoot again.  This class will take
+ * care of setting that delay and correctly decrementing the cooldown so that
+ * the weapon can shoot at the correct time again.  Additionally, this class
+ * will be used to handle the cooldown times for producing a unit or making
+ * a building.
+ * @author Ryan Fishel
+ *
+ */
+public class Interval {
+	
+	private int myMaxCooldown;
+	private int myCooldown;
+	
+	/**
+	 * Creates a new interval with a max cooldown.
+	 * @param cooldown is the initial max cooldwon of the interval
+	 */
+	public Interval (int cooldown) {
+		myMaxCooldown = cooldown;
+		myCooldown = 0;
+	}
+	
+	/**
+	 * Decreases the cooldown by 1 every time it is called if the cooldwon is
+	 * not already at 0.
+	 */
+	public void decrementCooldown() {
+		if(myCooldown != 0) {
+			myCooldown--;
+		}
+	}
+	
+	/**
+	 * Tells if the cooldown is over and an action is ready to be performed.
+	 * For example, if the interval is being used for a weapon, if this is 
+	 * true, is will signify that the weapon can shoot again.
+	 * @return true if the cooldown is at 0 and false if it is above zero.
+	 */
+	public boolean allowAction() {
+		return myCooldown == 0;
+	}
+	
+	/**
+	 * Resets the cooldown to the max cooldown.
+	 */
+	public void resetCooldown() {
+		myCooldown = myMaxCooldown;
+	}
+	
+	/**
+	 * Sets the max cooldown of the interval.  For example, if a weapon is
+	 * upgraded to have a shorter cooldown time this method will be used
+	 * to lower the max cooldown of the weapon
+	 * @param maxCooldown is the new max cooldown of the interval.
+	 */
+	public void setMaxCooldown(int maxCooldown) {
+		myMaxCooldown = maxCooldown;
+	}
+}
