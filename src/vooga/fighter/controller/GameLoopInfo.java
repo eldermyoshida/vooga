133dbe41d3587a65aa773670260af8d2daf104fd
Matt <mmparides91@gmail.com>
2013-04-14 14:33:08 -0400

    GameLoopInfo made for transferring paint data in main game loop

---
+++ src/vooga/fighter/controller/GameLoopInfo.java
@@ -0,0 +1,109 @@
+package vooga.fighter.controller;
+import java.awt.Dimension;
+import java.util.List;
+import java.util.ArrayList;
+
+import vooga.fighter.objects.utils.Health;
+import vooga.fighter.util.Location;
+import vooga.fighter.util.Pixmap;
+
+/**
+ * 
+ * @author matthewparides
+ *
+ */
+public class GameLoopInfo {
+	List<Location> mySpriteLocations;
+	List<Pixmap> mySprites;
+	List<Health> myHealthStats;
+	List<Dimension> myImageSizes;
+	
+	public GameLoopInfo() {
+		mySpriteLocations = new ArrayList<Location>();
+		mySprites = new ArrayList<Pixmap>();
+		myHealthStats = new ArrayList<Health>();
+		myImageSizes = new ArrayList<Dimension>();
+	}
+	
+	/**
+	 * @return the mySpriteLocations
+	 */
+	public List<Location> getSpriteLocations() {
+		return mySpriteLocations;
+	}
+	
+	/**
+	 * @param mySpriteLocations the mySpriteLocations to set
+	 */
+	public void setSpriteLocations(List<Location> mySpriteLocations) {
+		mySpriteLocations = mySpriteLocations;
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 */
+	public void setSpriteLocation(int index, Location loc) {
+		mySpriteLocations.set(index, loc);
+	}
+	
+	/**
+	 * @return the mySprites
+	 */
+	public List<Pixmap> getSprites() {
+		return mySprites;
+	}
+	
+	/**
+	 * @param mySprites the mySprites to set
+	 */
+	public void setSprites(List<Pixmap> mySprites) {
+		mySprites = mySprites;
+	}
+	
+	public void setSprite(int index, Pixmap pix) {
+		mySprites.set(index, pix);
+	}
+	
+	/**
+	 * @return the myHealthStats
+	 */
+	public List<Health> getHealthStats() {
+		return myHealthStats;
+	}
+	
+	/**
+	 * @param myHealthStats the myHealthStats to set
+	 */
+	public void setHealthStats(List<Health> myHealthStats) {
+		myHealthStats = myHealthStats;
+	}
+	
+	public void setHealthStat(int index, Health heal) {
+		myHealthStats.set(index, heal);
+	}
+	
+	/**
+	 * @return the myImageSizes
+	 */
+	public List<Dimension> getImageSizes() {
+		return myImageSizes;
+	}
+	
+	/**
+	 * @param myImageSizes the myImageSizes to set
+	 */
+	public void setImageSizes(List<Dimension> myImageSizes) {
+		myImageSizes = myImageSizes;
+	}
+	
+	/**
+	 * 
+	 */
+	public void setImageSize(int index, Dimension dim) {
+		myImageSizes.set(index, dim);
+	}
+	
+	
+	
+}
