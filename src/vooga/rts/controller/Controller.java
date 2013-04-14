0bc3a5f2d5f72c7a4862777f2c1504e45608db93
ChallenHB <crnhb38@gmail.com>
2013-04-14 17:34:15 -0400

    Figuring out how to pass input to Player

--- src/vooga/rts/controller/Controller.java
+++ src/vooga/rts/controller/Controller.java
@@ -11,21 +11,6 @@ import vooga.rts.input.ActionObject;
  */
 public interface Controller {
     
-    /**
-     * receiveInput gets the input from the Input class.
-     * The controller is responsible for doing any necessary operations on it.
-     * Originally, the Input called methods using the input, but having any
-     * changes take place in the controller allows us to alter it and modify it
-     * however we want without extra interaction with the Input. Furthermore, 
-     * we can now modify the list of commands, and store them within the controller 
-     * rather than within the Input.
-     * Note: Input class was changed so that it sends the input when it is notified as
-     * opposed to calling methods via reflection.
-     * 
-     * @param inputType
-     * @param action
-     */
-    public void receiveInput (String inputType, ActionObject action);
-    
+    public void sendCommand();
     
 }
--- src/vooga/rts/controller/Player.java
+++ src/vooga/rts/controller/Player.java
@@ -1,8 +1,11 @@
 package vooga.rts.controller;
 
+import javax.swing.JComponent;
 import vooga.rts.manager.Manager;
 import vooga.rts.input.ActionObject;
-
+import vooga.rts.input.AlertObject;
+import vooga.rts.input.Input;
+import vooga.rts.input.PositionObject;
 /**
  * At the moment, the player class is implementing the humanplayer. For network
  * player and ai player, some methods will be refactored into a super class
@@ -11,18 +14,20 @@ import vooga.rts.input.ActionObject;
  *
  */
 public class Player implements Controller {
+
+    private final static String DEFAULT_INPUT_LOCATION = "vooga.rts.resources.properties.Input";
     
     private Manager myManager;
     
-    public Player (Manager manager) {
+    public Player (Manager manager, JComponent component) {
         myManager = manager;
+        Input input = new Input(DEFAULT_INPUT_LOCATION, component);
+        input.addListenerTo(this);
     }
     
     public void sendCommand () { // this will send the command to the unit manager. Exactly what this is going to be is still being determined
         return;
     }
     
-    public void receiveInput (String action, ActionObject o) {
-        
-    }
+    //TODO: figure out how to have the same method called for all 
 }
