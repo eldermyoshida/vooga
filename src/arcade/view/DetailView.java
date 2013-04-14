77ef91e880731d7ba3d1f9ecef1336db9b5a5eb4
Will Nance <wllnance@gmail.com>
2013-04-14 14:46:58 -0400

    TODOs in detail view

--- src/arcade/view/DetailView.java
+++ src/arcade/view/DetailView.java
@@ -10,13 +10,24 @@ public class DetailView extends JFrame{
 	private ResourceBundle myResources;
 	private GameInfo myGameInfo;
 	
-	
+	/*
+	 * TODO:
+	 * I dont think we shouldpass resource bundle. THe resource bundle is in the game info.
+	 * If you need something in the resource bundle, we should hide that inside the gameinfo class
+	 */
 	public DetailView(GameInfo info, ResourceBundle resources){
 		myGameInfo= info;
 		myResources = resources;
 		
 		
-		
+	/*
+	 * TODO:
+	 * display ad screen from resource bundle
+	 * display description
+	 * display play button (if game is single player)
+	 * display multiplayer button (if game is multiplayer) 
+	 * 
+	 */
 		
 		
 	}
