b7b086398ebac2f4987dea636b494b07b86c46b7
ChallenHB <crnhb38@gmail.com>
2013-04-14 16:53:50 -0400

    Change to Player/Input interaction

--- src/vooga/rts/input/KeyboardModule.java
+++ src/vooga/rts/input/KeyboardModule.java
@@ -5,34 +5,34 @@ import java.awt.event.KeyListener;
 import javax.swing.JComponent;
 
 public class KeyboardModule extends InputDevice{
-	JComponent myComponent;
-	private Input myInput;
-	public final String myDevice = "KEYBOARD";
-	
-	public KeyboardModule(JComponent component, Input input){
-		super("Keyboard");
-		myInput = input;
-		myComponent = component;
-		initialize();
-	}
-	
-	private void initialize(){
-		myComponent.addKeyListener(new KeyListener() {
-			@Override
-			public void keyPressed(KeyEvent e){
-				String keyName = KeyboardHelper.getKeyName(e.getKeyCode());
-				myInput.actionNotification("Keyboard_" + keyName + "_KeyDown", new AlertObject(e.getWhen()));
-			}
+    JComponent myComponent;
+    private Input myInput;
+    public final String myDevice = "KEYBOARD";
 
-			@Override
-			public void keyReleased(KeyEvent e) {
-				String keyName = KeyboardHelper.getKeyName(e.getKeyCode());
-				myInput.actionNotification("Keyboard_" + keyName + "_KeyUp", new AlertObject(e.getWhen()));
-			}
+    public KeyboardModule(JComponent component, Input input){
+        super("Keyboard");
+        myInput = input;
+        myComponent = component;
+        initialize();
+    }
 
-			@Override
-			public void keyTyped(KeyEvent e) {				
-			}
-		});
-	}
+    private void initialize(){
+        myComponent.addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e){
+                String keyName = KeyboardHelper.getKeyName(e.getKeyCode());
+                myInput.actionNotification("Keyboard_" + keyName + "_KeyDown", new AlertObject(e.getWhen()));
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                String keyName = KeyboardHelper.getKeyName(e.getKeyCode());
+                myInput.actionNotification("Keyboard_" + keyName + "_KeyUp", new AlertObject(e.getWhen()));
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {				
+            }
+        });
+    }
 }
