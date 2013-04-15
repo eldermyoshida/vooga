17638c2dbb700164eb8d9c918d8b05ee526e2266
Jerry Li <th365thli@gmail.com>
2013-04-14 20:11:13 -0400

    integarting game and controller

--- src/vooga/fighter/controller/Main.java
+++ src/vooga/fighter/controller/Main.java
@@ -15,7 +15,7 @@ public class Main extends JFrame
 {
     // constants
     public static final Dimension SIZE = new Dimension(800, 600);
-    public static final String TITLE = "Mario!";
+    public static final String TITLE = "FightingGame";
 
 
     /**
