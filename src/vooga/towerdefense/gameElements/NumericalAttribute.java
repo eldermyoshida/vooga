624c48a7872c540577bfe15581e9e4f5161b9f98
Xu Rui <XuRui@wl-10-190-97-223.wireless.duke.edu>
2013-04-15 13:58:39 -0400

    merged numerical attribute with stat class

--- src/vooga/towerdefense/gameElements/NumericalAttribute.java
+++ src/vooga/towerdefense/gameElements/NumericalAttribute.java
@@ -1,6 +1,9 @@
 package vooga.towerdefense.gameElements;
 
 import java.awt.Graphics2D;
+
+import vooga.towerdefense.attributes.Attribute;
+import vooga.towerdefense.attributes.NumericalAttribute;
 import vooga.towerdefense.util.Location;
 
 
@@ -9,62 +12,81 @@ import vooga.towerdefense.util.Location;
  * Can be health, lives, damage level depending on nature of game element.
  * 
  * @author XuRui
+ * @author Mattew Roy
+ * 
  * 
  */
 
-public class Stat {
-    private final double myOriginalValue;
-    private String myName;
-    private double myCurrentValue;
-    private double regenRate;
-    private double myMaxValue;
+public class Stat extends Attribute {
+	private final double myOriginalValue;
+	private String myName;
+	private double myCurrentValue;
+	private double regenRate;
+	private double myMaxValue;
 
-    public Stat (String statName, double statValue) {
-        myName = statName;
-        myOriginalValue = statValue;
-        myCurrentValue = myOriginalValue;
-    }
+	public Stat (String statName, double statValue) {
+		myName = statName;
+		myOriginalValue = statValue;
+		myCurrentValue = myOriginalValue;
+	}
 
-    public void updateStat (double value) {
-        myCurrentValue = value;
-    }
+	public Double applyAttribute(NumericalAttribute toApply) {
+		modifyValue(toApply.getValue());
+		return myValue;
+	}
 
-    public void increment (double value) {
-        myCurrentValue += value;
-    }
+	/**
+	 * Replaces stat value with new value
+	 * @param value 
+	 */
+	public void updateStat (double value) {
+		myValue = value;
+	}
 
-    public void decrement (double value) {
-        myCurrentValue -= value;
-    }
+	/**
+	 * Adds the given parameter to the value.
+	 * Negative values subtract
+	 * 
+	 * @param valueToAdd 
+	 */
+	public void modifyValue (double valueToAdd) {
+		myValue += valueToAdd;
+	}
 
-    public String getDisplayableInfo () {
-        String info = myName + " : " + String.valueOf(myCurrentValue);
-        return info;
-    }
+	/**
+	 * Multiplies the current value. Values over 1 increase it.
+	 * Values < 1 decrease
+	 * 
+	 * @param multiplierToApply 
+	 */
+	public void applyMultiplier (double multiplierToApply) {
+		myValue *= multiplierToApply;
+	}
 
-    public double getValue () {
-        return myCurrentValue;
-    }
 
-    public String getName () {
-        return myName;
-    }
+	public void increment (double value) {
+		myCurrentValue += value;
+	}
 
-    /**
-     * 
-     * paints a bar representing this stat
-     */
-    public void paint (Graphics2D pen, Location where) {
-        // paints a bar representing this stat
-    }
+	public void decrement (double value) {
+		myCurrentValue -= value;
+	}
 
-    /**
-     * check whether this stat is different from its original value
-     * 
-     * @return
-     */
-    public boolean isChanged () {
-        return myOriginalValue != myCurrentValue;
-    }
+	/**
+	 * 
+	 * paints a bar representing this stat
+	 */
+	public void paint (Graphics2D pen, Location where) {
+		// paints a bar representing this stat
+	}
+
+	/**
+	 * check whether this stat is different from its original value
+	 * 
+	 * @return
+	 */
+	public boolean isChanged () {
+		return myOriginalValue != myCurrentValue;
+	}
 
 }
