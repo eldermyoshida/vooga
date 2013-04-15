526b1de3e5304a33953210251bd68d82ec7591f6
Gavin Ovsak <ovsak.gavin@gmail.com>
2013-04-15 14:41:48 -0400

    Input v2.0
    See Included Documentation for new Features

--- src/util/inputExample/Pixmap.java
+++ src/util/inputExample/Pixmap.java
@@ -1,13 +1,14 @@
-package util;
+package util.inputExample;
 
-import java.awt.Graphics2D;
 import java.awt.Dimension;
-import java.awt.Image;
-import java.awt.geom.Point2D;
+import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import vooga.fighter.util.Paintable;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 /**
  * This class represents an image on the screen and 
@@ -15,19 +16,17 @@ import vooga.fighter.util.Paintable;
  * 
  * Note, Java only supports the formats: png, jpg, gif.
  * 
- * @author Robert C. Duvall, Dagbedji F
- * Added get icon
+ * @author Robert C. Duvall, Gavin Ovsak
  */
-public class Pixmap implements Paintable {
-    // OS-independent relative resource locations (like URLs)
-    private static final String RESOURCE_LOCATION = "/vooga/";
+public class Pixmap {
     // underlying implementation
-    private java.awt.Image myImage;
+    private BufferedImage myImage;
     private String myFileName;
 
 
     /**
      * Create an image from the given filename.
+     * @param fileName is the file of image
      */
     public Pixmap (String fileName) {
         setImage(fileName);
@@ -35,6 +34,7 @@ public class Pixmap implements Paintable {
 
     /**
      * Create a copy of image from the given other image.
+     * @param other 
      */
     public Pixmap (Pixmap other) {
         this(other.myFileName);
@@ -42,14 +42,20 @@ public class Pixmap implements Paintable {
 
     /**
      * Set this image to the image referred to by the given filename.
+     * @param fileName 
      */
     public void setImage (String fileName) {
-        myImage = new ImageIcon(getClass().getResource(RESOURCE_LOCATION + fileName)).getImage();
+    	try {
+			myImage = ImageIO.read(new File("src/util/inputExample/" + fileName));
+		} catch (IOException e) {}
         myFileName = fileName;
     }
 
     /**
      * Describes how to draw the image on the screen.
+     * @param pen 
+     * @param center 
+     * @param size 
      */
     public void paint (Graphics2D pen, Point2D center, Dimension size) {
         paint(pen, center, size, 0);
@@ -57,6 +63,12 @@ public class Pixmap implements Paintable {
 
     /**
      * Describes how to draw the image rotated on the screen.
+     * 
+     * angle is in degrees
+     * @param pen 
+     * @param center 
+     * @param size 
+     * @param angle 
      */
     public void paint (Graphics2D pen, Point2D center, Dimension size, double angle) {
         // save current state of the graphics area
@@ -64,11 +76,10 @@ public class Pixmap implements Paintable {
         // move graphics area to center of this shape
         pen.translate(center.getX(), center.getY());
         // rotate area about this shape
-        pen.rotate(angle);
+        pen.rotate(Math.toRadians(angle));
         // draw as usual (i.e., rotated)
         pen.drawImage(myImage, -size.width / 2, -size.height / 2, size.width, size.height, null);
         // restore graphics area to its old state, so our changes have no lasting effects
-		pen.setTransform(old);
-	}
-
+        pen.setTransform(old);
+    }
 }
