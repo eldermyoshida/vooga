90f6fb18263185658a922ac679a545f3680bb07b
Will Nance <wllnance@gmail.com>
2013-04-15 13:31:24 -0400

    implemented play game for example game

--- src/games/example/Example.java
+++ src/games/example/Example.java
@@ -18,7 +18,7 @@ public class Example extends Game {
 
     @Override
     public void run () {
-        // TODO Auto-generated method stub
+        System.out.println("running the game");
 
     }
 
