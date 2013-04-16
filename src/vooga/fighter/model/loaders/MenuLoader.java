17d0e2c700f0e08183d8bcfb4ae3d81fe92a270e
Jack Matteucci <Jackmatt16@gmail.com>
2013-04-16 17:58:07 -0400

    Added nonfunctional MenuLoader to the mix, working on getting it to work

---
+++ src/vooga/fighter/model/loaders/MenuLoader.java
@@ -0,0 +1,16 @@
+package vooga.fighter.model.loaders;
+
+public class MenuLoader extends ObjectLoader {
+
+	public MenuLoader(String objectPath) {
+		super(objectPath);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void load(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
