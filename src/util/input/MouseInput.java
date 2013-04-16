35a97488d0bff60603882434a0cf84c3f19a12ac
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-04-16 19:36:57 -0400

    Revert "tried to get input to work - input team has not finished coding so it doesn't work."
    This reverts commit 068bb315856e957f4351088389127cc0ce1e5a5d.

--- src/util/input/MouseInput.java
+++ src/util/input/MouseInput.java
@@ -7,9 +7,11 @@ import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import javax.swing.JComponent;
 
 /**
