21525e8e1974986a65752df9794a4bedd49b34cf
Erick Gonzalez <erick.gonzal3z@gmail.com>
2013-04-17 01:46:44 -0400

    added trolls

--- src/vooga/towerdefense/factories/TrollUnitDefinition.java
+++ src/vooga/towerdefense/factories/TrollUnitDefinition.java
@@ -1,5 +1,6 @@
 package vooga.towerdefense.factories;
 
+
 import java.awt.Dimension;
 import vooga.towerdefense.util.Location;
 import vooga.towerdefense.util.Pixmap;
@@ -11,9 +12,9 @@ import vooga.towerdefense.util.Pixmap;
  */
 public class TrollUnitDefinition extends GameElementDefinition {
     
-    private static final Pixmap TOWER_IMAGE = new Pixmap("Troll_Sprite.png");
+    private static final Pixmap TROLL_IMAGE = new Pixmap("Troll_Sprite.png");
     private static final Location DEFAULT_TOWER_LOCATION = new Location(200, 100);
-    private static final Dimension DEFAULT_TOWER_SIZE = new Dimension(50, 50);
+    private static final Dimension DEFAULT_TOWER_SIZE = new Dimension(150, 150);
     private static final double DEFAULT_MOVE_SPEED = 50;
     private static final double DEFAULT_DIRECTION = 50;
     
@@ -25,7 +26,13 @@ public class TrollUnitDefinition extends GameElementDefinition {
      * 
      */
     public TrollUnitDefinition () {
-        // TODO Auto-generated constructor stub
+        myImage = TROLL_IMAGE;
+        myCenter = DEFAULT_TOWER_LOCATION;
+        mySize = DEFAULT_TOWER_SIZE;
+    }
+    
+    public Dimension getSize() {
+        return DEFAULT_TOWER_SIZE;
     }
 
     
--- src/vooga/towerdefense/gameElements/GameElement.java
+++ src/vooga/towerdefense/gameElements/GameElement.java
@@ -51,11 +51,11 @@ public class GameElement extends Sprite {
     }
 
     public void update (double elapsedTime) {
-        /*
-         * for (Action a : myActions) {
-         * a.update(elapsedTime, event);
-         * }
-         */
+        
+          for (Action a : myActions) {
+          a.update(elapsedTime);
+          }
+         
     }
     
     public void addActions(List<Action> actions) {
