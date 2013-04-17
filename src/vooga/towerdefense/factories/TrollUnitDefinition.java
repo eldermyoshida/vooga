4b0c4d11377147b41d49315d0e8a9e93772b7d9f
Matthew Roy <matthewaroy@gmail.com>
2013-04-16 23:18:44 -0400

    Created test unit Troll for use in front end testing. Fixed unit definitions, anded more to factories

---
+++ src/vooga/towerdefense/factories/TrollUnitDefinition.java
@@ -0,0 +1,30 @@
+package vooga.towerdefense.factories;
+
+import java.awt.Dimension;
+import util.Pixmap;
+import vooga.towerdefense.util.Location;
+
+/**
+ * @author Matthew Roy
+ *
+ */
+public class TrollUnitDefinition extends UnitDefinition {
+    
+    private static final Pixmap TOWER_IMAGE = new Pixmap("Troll_Sprite.png");
+    private static final Location DEFAULT_TOWER_LOCATION = new Location(200, 100);
+    private static final Dimension DEFAULT_TOWER_SIZE = new Dimension(50, 50);
+    
+    public Pixmap myImage;
+    public Location myCenter;
+    public Dimension mySize;
+
+    /**
+     * 
+     */
+    public TrollUnitDefinition () {
+        // TODO Auto-generated constructor stub
+    }
+
+    
+    
+}
---
+++ src/vooga/towerdefense/factories/UnitDefinition.java
@@ -0,0 +1,16 @@
+package vooga.towerdefense.factories;
+
+/**
+ * @author Matthew Roy
+ *
+ */
+public class UnitDefinition extends GameElementDefinition {
+
+    /**
+     * 
+     */
+    public UnitDefinition () {
+        // TODO Auto-generated constructor stub
+    }
+
+}
