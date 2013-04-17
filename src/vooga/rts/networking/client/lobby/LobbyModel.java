cde60fa7c438456c71a6378efb40c21358d73122
srwareham <srwareham@hotmail.com>
2013-04-17 12:26:43 -0400

    Added more networking-needed features for lobbys
    Added remove players, move player…etc
    
    Move player, remove player may need diferent implementations depending
    on how Players are passed down to the lobby
    
    Skeleton class for LobbyView

--- src/vooga/rts/networking/client/lobby/LobbyModel.java
+++ src/vooga/rts/networking/client/lobby/LobbyModel.java
@@ -65,6 +65,53 @@ public class LobbyModel {
             //is called internally and this error should never occur
         }
     }
+    
+    /**
+     * Provided a player to remove, this method searches through the existing teams and
+     * removes said player from its team.  If it is not a member of a team Should not occur) nothing happens
+     * @param player
+     */
+    public void removePlayer(Player player) {
+        Team team = getTeam(player);
+        if (team != null) {
+            team.removePlayer(player);
+        }
+    }
+    
+    /**
+     * Returns the Parent team of an input player. Returns null if player
+     * is not a member of any team.
+     * @param player
+     * @return
+     */
+    public Team getTeam(Player player) {
+        for (Team team: myTeams) {
+            if (team.contains(player)) {
+                return team;
+            }
+        }
+            return null;
+    }
+    
+    /**
+     * Method to remove a player from its current team, and add it to the 
+     * desired new team
+     * Note: perhaps wil need some sort of collection of connected peers
+     * from which the move method will not remove a player so that
+     * they are constantly considered connected.  Calling remove player
+     * may become an issue, this implementation may need to change
+     * if removing a player is to cut connections with them.
+     * @param player
+     * @param newTeam
+     */
+    public void movePlayer(Player player, Team newTeam) {
+        removePlayer(player);
+        newTeam.addPlayer(player);
+    }
+    
+    // perhaps need a method here that talks to the server checking who is still conencted
+    //if someone disconnects, they will be removed from the team
+    
     /**
      * This method returns whether or not the lobby is full.
      * 
@@ -79,6 +126,23 @@ public class LobbyModel {
         }
         return true;
     }    
+    
+    /**
+     * Returns the name of the lobby.
+     * @return
+     */
+    public String getName() {
+        return myLobbyName;
+    }
+    
+    /**
+     * Returns the name of the current Map.
+     * Note: perhaps will actually return a map object instead, if desired
+     * @return
+     */
+    public String getMapName() {
+        return myMapName;
+    }
     
     //TODO:  Either need a method to actually start the game by passing all the necessary constructors,
     //or some sort of boolean that states that the game is ready to be played, so come find my relevant information
