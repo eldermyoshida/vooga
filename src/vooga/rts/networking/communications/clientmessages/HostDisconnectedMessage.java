e7a59c01c5ac945705fff81639fcac831d96834f
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-04-17 17:13:57 -0400

    Removed error method in networkig

--- src/vooga/rts/networking/communications/clientmessages/HostDisconnectedMessage.java
+++ src/vooga/rts/networking/communications/clientmessages/HostDisconnectedMessage.java
@@ -3,5 +3,5 @@ package vooga.rts.networking.communications.clientmessages;
 public class HostDisconnectedMessage {
     private String myUsername;
     
-    public HostDisconnected 
+    //public HostDisconnected 
 }
