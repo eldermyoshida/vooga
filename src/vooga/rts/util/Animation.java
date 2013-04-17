c3b16701a2f23100b36c8ecc22e5e8e662bcc31f
jo63 <jo63@duke.edu>
2013-04-17 03:18:07 -0400

    initial commit to animation branch, created animation class

---
+++ src/vooga/rts/util/Animation.java
@@ -0,0 +1,15 @@
+package vooga.rts.util;
+
+
+/**
+ * This class handles with the animation of images either through a sprite sheet or some other file format
+ * 
+ * @author junho oh
+ *
+ */
+public class Animation {
+
+    public Animation () {
+    }
+
+}
