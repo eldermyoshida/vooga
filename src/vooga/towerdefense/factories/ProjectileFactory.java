027d9d85eb0a8c6411da88011e562938024bbf83
Zhen Gou <zg20@duke.edu>
2013-04-17 21:11:53 -0400

    refactoring projectile factory

--- src/vooga/towerdefense/factories/ProjectileFactory.java
+++ src/vooga/towerdefense/factories/ProjectileFactory.java
@@ -9,16 +9,15 @@ import vooga.towerdefense.attributes.AttributeConstants;
 import vooga.towerdefense.attributes.AttributeManager;
 import vooga.towerdefense.gameElements.GameElement;
 import vooga.towerdefense.gameElements.Projectile;
-import vooga.towerdefense.util.Location;
 import vooga.towerdefense.util.Pixmap;
 
 public class ProjectileFactory {
-	private static final AttributeConstants myAttributeConstants=new AttributeConstants();
+	private static final AttributeConstants ATTRIBUTES_CONSTANTS=new AttributeConstants();
     private static int DEFAULT_WIDTH;
     private static final Pixmap DEFAULT_IMAGE = new Pixmap("defined by designer");
     private static final Dimension DEFAULT_SIZE = new Dimension(DEFAULT_WIDTH, DEFAULT_WIDTH);
     private static final List<Action> DEFAULT_ACTIONS = new ArrayList<Action>();
-    private AttributeManager DEFAULT_ATTRIBUTE_MANAGER;
+    private static final AttributeManager DEFAULT_ATTRIBUTE_MANAGER=new AttributeManager();
     
     
     
