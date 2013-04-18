5b4d5bfb08ceba393ba6267dd82ca10658d8c89f
jo63 <jo63@duke.edu>
2013-04-17 22:35:37 -0400

    added a test sprite, started working on Animation

--- src/vooga/rts/util/Animation.java
+++ src/vooga/rts/util/Animation.java
@@ -1,5 +1,8 @@
 package vooga.rts.util;
 
+import java.awt.image.BufferedImage;
+import vooga.rts.resourcemanager.ResourceManager;
+
 
 /**
  * This class handles with the animation of images either through a sprite sheet or some other file format
@@ -8,8 +11,20 @@ package vooga.rts.util;
  *
  */
 public class Animation {
-
-    public Animation () {
+    private Pixmap mySheet;
+    private String myFileName;
+    public Animation (String fileName) {
+        myFileName = fileName;
+        loadFile(myFileName);
+    }
+    
+    private void loadFile(String fileName) {
+        mySheet = new Pixmap(ResourceManager.getInstance()
+                            .<BufferedImage> getFile("fileName", BufferedImage.class));
+        //mySheet.pa
+        
+        
+        
     }
 
 }
