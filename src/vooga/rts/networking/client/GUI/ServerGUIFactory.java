0af9d8ca8a06904940ece51f770ba363bbe592be
Henrique Moraes <hrm8@duke.edu>
2013-04-17 22:13:25 -0400

    finished jd

--- src/vooga/rts/networking/client/GUI/ServerGUIFactory.java
+++ src/vooga/rts/networking/client/GUI/ServerGUIFactory.java
@@ -12,14 +12,28 @@ import javax.swing.border.EmptyBorder;
 import vooga.rts.networking.communications.Message;
 import vooga.rts.networking.communications.clientmessages.HostDescriptionMessage;
 
+/**
+ * Class that is responsible for creating some panels for the GUI
+ * Singleton
+ * @author Henrique Moraes, Ellango Jothimurugesan
+ *
+ */
 public class ServerGUIFactory{
     private static final Font DEFAULT_FONT = new Font("Century Gothic", Font.PLAIN,18);
     private Font myFont = DEFAULT_FONT;
     private static ServerGUIFactory instance;
     
+    /**
+     * dulcao do cana?
+     * private constructor of this class
+     */
     private ServerGUIFactory(){
     }
     
+    /**
+     * 
+     * @return instance of this class
+     */
     public static ServerGUIFactory getInstance(){
         if (instance == null) {
             instance = new ServerGUIFactory();
@@ -27,8 +41,12 @@ public class ServerGUIFactory{
         return instance;
     }
     
-    public JPanel createJoinPanel(Message message){
-        HostDescriptionMessage host = (HostDescriptionMessage) message;
+    /**
+     * 
+     * @param message host description for this host
+     * @return Panel containing host information
+     */
+    public JPanel createJoinPanel(HostDescriptionMessage message){
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BorderLayout(10,10));
         
@@ -36,9 +54,9 @@ public class ServerGUIFactory{
         generalInfo.setLayout(new GridLayout(2,1,0,20));
         generalInfo.setOpaque(false);
         
-        Image scaled = ImageHelper.getScaledImage(host.getImagePath(), 150);
+        Image scaled = ImageHelper.getScaledImage(message.getImagePath(), 150);
 
-        JLabel mapLabel = makeLabel(host.getMapDescription());
+        JLabel mapLabel = makeLabel(message.getMapDescription());
         mapLabel.setHorizontalTextPosition(SwingConstants.CENTER);
         mapLabel.setVerticalTextPosition(SwingConstants.TOP);
         mapLabel.setAlignmentX(SwingConstants.TOP);
@@ -50,9 +68,9 @@ public class ServerGUIFactory{
         description.setOpaque(false);
         generalInfo.add(description);
         description.setLayout(new GridLayout(0,1));
-        description.add(makeLabel(host.getServerName()));
-        description.add(makeLabel(host.getHost()));
-        description.add(makeLabel(host.getMaxPlayers()+""));
+        description.add(makeLabel(message.getServerName()));
+        description.add(makeLabel(message.getHost()));
+        description.add(makeLabel(message.getMaxPlayers()+""));
         
         mainPanel.add(generalInfo, BorderLayout.WEST);
         mainPanel.add(createSpecificPanel(), BorderLayout.CENTER);
@@ -60,16 +78,29 @@ public class ServerGUIFactory{
         return mainPanel;
     }
     
+    /**
+     * 
+     * @param desc description of the label
+     * @return a label set with this factory's font
+     */
     private JLabel makeLabel(String desc) {
         JLabel label = new JLabel(desc);
         label.setFont(myFont);
         return label;
     }
     
+    /**
+     * Sets the font for this factory's labels
+     * @param font the font to be used
+     */
     public void setFont(Font font){
         myFont = font;
     }
     
+    /**
+     * 
+     * @return panel specific for host description
+     */
     private JPanel createSpecificPanel(){
         JPanel specific = new JPanel();
         int numLabels = 6;
