d0d57584165fc96a3c6cc01ba1ba340deb07daac
xurui203 <xurui203@gmail.com>
2013-04-18 23:35:05 -0400

    tracktarget action fully implemented

--- src/vooga/towerdefense/action/TrackTarget.java
+++ src/vooga/towerdefense/action/TrackTarget.java
@@ -1,6 +1,7 @@
 package vooga.towerdefense.action;
 
 import vooga.towerdefense.attributes.Attribute;
+import vooga.towerdefense.attributes.AttributeConstants;
 import vooga.towerdefense.gameElements.GameElement;
 import vooga.towerdefense.util.Location;
 import vooga.towerdefense.util.Vector;
@@ -9,19 +10,37 @@ public class TrackTarget extends Action{
 	
 	private Vector myHeading;
 	private Location myStart;
+	private Location myTarget;
+	private Attribute myMoveSpeed;
+	private boolean following;
 
-	public TrackTarget(GameElement initiator, Location destination){
+	public TrackTarget(GameElement initiator, Location destination, Attribute movespeed){
 		super(initiator);
+		myMoveSpeed = movespeed;
+		following = true;
 		myStart = initiator.getCenter();
+		myTarget = destination;
 		myHeading = new Vector(Vector.angleBetween(myStart, destination), 
-				initiator.getAttributeManager().getAttribute(Attribute.ATTRIBUTE_CONSTANTS.MOVE_SPEED).getValue());
+				initiator.getAttributeManager().getAttribute(AttributeConstants.MOVE_SPEED).getValue());
+	}
+	
+	public void update(double elapsedTime) {
+		if(following) {
+			executeAction(elapsedTime);
+		}
 	}
 
 	@Override
-	public void executeAction(double elapseTime) {
-		while (Vector.distanceBetween(myStart, getInitiator().getCenter()) <= 
-				getInitiator().getAttributeManager().getAttribute(Attribute.ATTRIBUTE_CONSTANTS.ATTACK_RADIUS).getValue()){
-				getInitiator().translate(myHeading);
+	public void executeAction(double elapsedTime) {
+		myHeading = new Vector(myStart, myTarget);
+		myHeading.scale(myHeading.getMagnitude()/myMoveSpeed.getValue());
+		myHeading.scale(elapsedTime);
+		if (myStart.distance(myTarget) < myHeading.getMagnitude()) {
+			following = false;
+			myStart.setLocation(myTarget);
+		}
+		else {
+			myStart.translate(myHeading);
 		}
 	}
 	
