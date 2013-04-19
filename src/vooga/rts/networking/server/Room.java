ca9a4737da1917b0df10d74d993166cdf4fdacb0
David Winegar <david.s.winegar@gmail.com>
2013-04-19 00:13:18 -0400

    Finished commenting up server

--- src/vooga/rts/networking/server/Room.java
+++ src/vooga/rts/networking/server/Room.java
@@ -1,18 +1,34 @@
 package vooga.rts.networking.server;
 
 /**
- * This is the superclass for Lobby and GameServer. It represents a small 
+ * This is the superclass for Lobby and GameServer. It represents a small
+ * 
  * @author David Winegar
- *
+ * 
  */
 public class Room extends AbstractThreadContainer {
     private GameContainer myGameContainer;
     private int myID;
 
+    /**
+     * Creates a room with ID and GameContainer.
+     * 
+     * @param id room number
+     * @param container GameContainer
+     */
     public Room (int id, GameContainer container) {
         setIDandContainer(id, container);
     }
-    
+
+    /**
+     * Creates a Room with ID and GameContainer while copying in all current connections from the
+     * other room, removing that room's connections, removing that room from the container, and
+     * adding this room to the container.
+     * 
+     * @param id room number
+     * @param container GameContainer
+     * @param room room to replace
+     */
     public Room (int id, GameContainer container, Room room) {
         super(room);
         setIDandContainer(id, container);
@@ -20,18 +36,18 @@ public class Room extends AbstractThreadContainer {
         container.removeRoom(room);
         container.addRoom(this);
     }
-    
+
     private void setIDandContainer (int id, GameContainer container) {
         myID = id;
         myGameContainer = container;
     }
-    
+
     protected GameContainer getGameContainer () {
         return myGameContainer;
     }
-    
+
     protected int getID () {
         return myID;
     }
-    
+
 }
