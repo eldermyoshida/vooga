8d80f544a367178ce54de9f352a619b17580bc71
Angelica <angeschwartz@gmail.com>
2013-04-19 01:44:36 -0400

    started work on visual aspect of game editor

---
+++ src/vooga/towerdefense/gameeditor/GameEditor.java
@@ -0,0 +1,29 @@
+package vooga.towerdefense.gameeditor;
+
+import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import javax.swing.JFrame;
+
+public class GameEditor extends JFrame {
+
+    private Dimension mySize;
+    private MouseAdapter myMouseAdapter;
+    
+    public GameEditor(Dimension size) {
+        mySize = size;
+        setSize(mySize);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        makeMouseAdapter();
+        addMouseListener(myMouseAdapter);
+        createGUI();
+    }
+    
+    public void createGUI() {
+        StartUpScreen screen = new StartUpScreen(mySize);
+        add(screen);
+        screen.display();
+    }
+}
---
+++ src/vooga/towerdefense/gameeditor/Main.java
@@ -0,0 +1,10 @@
+package vooga.towerdefense.gameeditor;
+
+import java.awt.Dimension;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GameEditor editor = new GameEditor(new Dimension(1000, 800));
+    }
+}
