28f86498037d089e99fac0bc7d18304632cab1a5
ej48 <ej48@duke.edu>
2013-04-19 12:24:28 -0400

    added form class, username and password now show up in register view

--- src/arcade/view/TextKeywords.java
+++ src/arcade/view/TextKeywords.java
@@ -12,12 +12,14 @@ public interface TextKeywords {
     static final String LOGIN = "login";
     static final String REGISTER = "new_account";
     static final String USERNAME = "username";
+    static final String USERNAME_ERROR = "username_error";
     static final String PASSWORD = "password";
     static final String REGISTER_MESSAGE = "new_account_instructions";
     static final String FIRSTNAME = "firstname";
     static final String LASTNAME = "lastname";
     static final String BIRTHDATE = "birthdate";
     static final String BIRTHDATE_MESSAGE = "birthdate_format";
+    static final String BIRTHDATE_ERROR = "birthdate_error";
     static final String IMAGE = "images";
     static final String IMAGE_MESSAGE = "image_instructions";
     static final String IMAGE_BUTON = "image_button";
