460a20daeb275559accac89b90b9b82b1680b544
Ryan Fishel <fishel.ryan@gmail.com>
2013-04-20 19:50:21 -0400

    Painting worker to gui

--- src/vooga/rts/state/GameState.java
+++ src/vooga/rts/state/GameState.java
@@ -211,6 +211,7 @@ public class GameState extends SubState implements Controller {
         addPlayer(1);
         myHumanPlayer.add(new Soldier());
         myHumanPlayer.add(new Soldier(new Location3D(200, 200, 0)));
+        myHumanPlayer.add(new Worker(new Pixmap(ResourceManager.getInstance().<BufferedImage>getFile("images/scv.gif", BufferedImage.class)), new Location3D(500, 400, 0), new Dimension(75, 75), null, 1, 300, 40, 100));
         myHumanPlayer.add(new Barracks(new Pixmap(ResourceManager.getInstance().<BufferedImage>getFile("images/factory.png", BufferedImage.class)), new Location3D(1000, 500, 0), new Dimension(200,  200), null, 1, 100, 100));
         myMap = new GameMap(8, new Dimension(512, 512));
     }
