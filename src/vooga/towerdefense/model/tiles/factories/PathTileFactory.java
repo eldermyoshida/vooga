efb76f461750a26573dd70fe56a2124433a86576
Erick Gonzalez <erick.gonzal3z@gmail.com>
2013-04-19 22:39:52 -0400

    Added PathTileFactory.java used to create Path Tiles

---
+++ src/vooga/towerdefense/model/tiles/factories/PathTileFactory.java
@@ -0,0 +1,20 @@
+package vooga.towerdefense.model.tiles.factories;
+
+import java.awt.Dimension;
+import vooga.towerdefense.model.tiles.Tile;
+import vooga.towerdefense.util.Location;
+import vooga.towerdefense.util.Pixmap;
+
+/**
+ * A tile factory that creates PathTile objects.
+ * 
+ * @author Erick Gonzalez
+ */
+public class PathTileFactory extends TileFactory {
+
+    @Override
+    public Tile createTile (int id, Pixmap image, Location center, Dimension size) {
+        return null;
+    }
+
+}
