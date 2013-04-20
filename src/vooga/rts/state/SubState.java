337f008a53251f6809989dd7210b7ec811823904
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-04-20 15:12:00 -0400

    Removed all the original files, deleted input from our code

--- src/vooga/rts/state/SubState.java
+++ src/vooga/rts/state/SubState.java
@@ -1,12 +1,10 @@
- package vooga.rts.state;
+package vooga.rts.state;
 
 import java.util.Observable;
 import java.util.Observer;
-import vooga.rts.commands.Command;
-import vooga.rts.IGameLoop;
 
 public abstract class SubState extends Observable implements State {
-    
+
     public SubState (Observer observer) {
         addObserver(observer);
     }
