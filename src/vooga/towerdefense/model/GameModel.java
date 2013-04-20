bf8033c7fad6708166874f5896e97eeb5adb189a
Erick Gonzalez <erick.gonzal3z@gmail.com>
2013-04-19 21:31:24 -0400

    Added some documentation to GameModel.java

--- src/vooga/towerdefense/model/GameModel.java
+++ src/vooga/towerdefense/model/GameModel.java
@@ -3,11 +3,7 @@ package vooga.towerdefense.model;
 import java.awt.Graphics2D;
 import java.awt.Point;
 import java.util.List;
-
 import vooga.towerdefense.controller.Controller;
-import vooga.towerdefense.factories.ExampleUnitFactory;
-import vooga.towerdefense.factories.TrollUnitDefinition;
-import vooga.towerdefense.factories.WaveFactory;
 import vooga.towerdefense.gameElements.Wave;
 import vooga.towerdefense.shop.Shop;
 import vooga.towerdefense.shop.ShopItem;
@@ -20,34 +16,56 @@ import vooga.towerdefense.shop.ShopItem;
  * @author Jimmy Longley
  * @author Erick Gonzalez
  */
-public class GameModel {    
+public class GameModel {
     private Controller myController;
     private List<Wave> myWaves;
     private GameMap myGameMap;
     private Wave myCurrentWave;
     private Shop myShop;
 
-    public GameModel(Controller controller, List<Wave> waves, GameMap gameMap, Shop shop) {
+    /**
+     * 
+     * @param controller a controller
+     * @param waves a list of wave objects
+     * @param gameMap a game map
+     * @param shop a shop
+     */
+    public GameModel (Controller controller, List<Wave> waves, GameMap gameMap, Shop shop) {
         myController = controller;
         myWaves = waves;
         myGameMap = gameMap;
         myShop = shop;
-		myCurrentWave = waves.get(0);
+        myCurrentWave = waves.get(0);
     }
-    
-    public Tile getTile(Point p) {
+
+    /**
+     * Given a point p, returns the tile object that contains this point.
+     * 
+     * @param p a point
+     * @return a tile at the given point
+     */
+    public Tile getTile (Point p) {
         return myGameMap.getTile(p);
     }
 
+    /**
+     * Updates the game during an iteration of the game loop.
+     * 
+     * @param elapsedTime time elapsed since last clock tick
+     */
     public void update (double elapsedTime) {
         updateWave(elapsedTime);
         myGameMap.update(elapsedTime);
     }
 
+    /**
+     * 
+     * @param elapsedTime
+     */
     public void updateWave (double elapsedTime) {
-		myCurrentWave.update(elapsedTime);
-		if (myCurrentWave.waveCompleted())
-			startNextWave();
+        myCurrentWave.update(elapsedTime);
+        if (myCurrentWave.waveCompleted())
+            startNextWave();
     }
 
     public void startNextWave () {
@@ -59,20 +77,40 @@ public class GameModel {
             System.out.println("you win!");
         }
     }
-    
-    public void paintMap(Graphics2D pen) {
+
+    /**
+     * paints the map
+     * 
+     * @param pen a pen
+     */
+    public void paintMap (Graphics2D pen) {
         myGameMap.paint(pen);
     }
 
+    /**
+     * 
+     * @return a GameMap object
+     */
     public GameMap getMap () {
         return myGameMap;
     }
-    
-    public void paintShop(Graphics2D pen) { 
+
+    /**
+     * Paints the shop.
+     * 
+     * @param pen a pen
+     */
+    public void paintShop (Graphics2D pen) {
         myShop.paint(pen);
     }
-    
-    public ShopItem getShopItem(Point p) {
+
+    /**
+     * Given a point p, retrieves the shop item at this point.
+     * 
+     * @param p a point
+     * @return the ShopItem at the given point p.
+     */
+    public ShopItem getShopItem (Point p) {
         return myShop.getShopItem(p);
     }
 }
