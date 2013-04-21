304d9f6d0985bb54689d911edc976b423085dc98
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-04-21 01:57:51 -0400

    Formatted like my Sunflower

--- src/vooga/rts/gamedesign/strategy/production/CanProduce.java
+++ src/vooga/rts/gamedesign/strategy/production/CanProduce.java
@@ -7,11 +7,11 @@ import vooga.rts.gamedesign.sprite.gamesprites.interactive.InteractiveEntity;
 import vooga.rts.gamedesign.sprite.gamesprites.interactive.buildings.ProductionBuilding;
 import vooga.rts.util.DelayedTask;
 import vooga.rts.util.Location3D;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+
 
 /**
  * This class implements ProductionStrategy and is used as an instance in
@@ -24,50 +24,48 @@ import java.util.Observer;
  */
 public class CanProduce implements IProducer {
 
-	private List<InteractiveEntity> myProducables;
-	private Location3D myRallyPoint;
+    private List<InteractiveEntity> myProducables;
+    private Location3D myRallyPoint;
 
-	public CanProduce() {
-		myProducables = new ArrayList<InteractiveEntity>();
-		myRallyPoint = new Location3D();
-	}
+    public CanProduce () {
+        myProducables = new ArrayList<InteractiveEntity>();
+        myRallyPoint = new Location3D();
+    }
 
-	public void setRallyPoint(Location3D rallyPoint) {
-		myRallyPoint = rallyPoint;
-	}
+    public void setRallyPoint (Location3D rallyPoint) {
+        myRallyPoint = rallyPoint;
+    }
 
-	public void addProducable(InteractiveEntity producable) {
-		myProducables.add(producable);
-	}
+    public void addProducable (InteractiveEntity producable) {
+        myProducables.add(producable);
+    }
 
-	@Override
-	public void createProductionActions(final InteractiveEntity producer) {
-		if (producer instanceof ProductionBuilding) {
+    @Override
+    public void createProductionActions (final InteractiveEntity producer) {
+        if (producer instanceof ProductionBuilding) {
 
-			for (final InteractiveEntity producable : myProducables) {
+            for (final InteractiveEntity producable : myProducables) {
 
-				producer.addAction("I am a pony", new InteractiveAction(
-						producer) {
-					@Override
-					public void update(Command command) {
-					}
+                producer.addAction("I am a pony", new InteractiveAction(producer) {
+                    @Override
+                    public void update (Command command) {
+                    }
 
-					@Override
-					public void apply() {
-						// check for resources
-						final InteractiveEntity unit = producable;
-						DelayedTask dt = new DelayedTask(1, new Runnable() {
-							@Override
-							public void run() {
-								producer.notifyObservers(((InteractiveEntity) unit)
-										.copy());
+                    @Override
+                    public void apply () {
+                        // check for resources
+                        final InteractiveEntity unit = producable;
+                        DelayedTask dt = new DelayedTask(1, new Runnable() {
+                            @Override
+                            public void run () {
+                                producer.notifyObservers(((InteractiveEntity) unit).copy());
 
-							}
-						});
-						producer.addTask(dt);
-					}
-				});
-			}
-		}
-	}
+                            }
+                        });
+                        producer.addTask(dt);
+                    }
+                });
+            }
+        }
+    }
 }
