6015c3653de9639707d9c986a9be48962dce2042
David Winegar <david.s.winegar@gmail.com>
2013-04-20 21:56:57 -0400

    Edited the view and adapter for the server browser

--- src/vooga/rts/networking/client/GUI/ServerBrowserTableAdapter.java
+++ src/vooga/rts/networking/client/GUI/ServerBrowserTableAdapter.java
@@ -5,16 +5,20 @@ import javax.swing.table.AbstractTableModel;
 
 /**
  * Adapter between the ServerBrowserView's table and the data from the server.
+ * TODO connect with actual model
  * 
  * @author David Winegar
  * 
  */
 public class ServerBrowserTableAdapter extends AbstractTableModel {
-    private String[] myColumnNames = { "Server Name",
-                                    "Map",
-                                    "Players" };
+    private static final long serialVersionUID = -4753703508648678535L;
 
-    private int[][] myTest = {{3, 3, 3}, {4, 4, 4}, {5, 5, 5}};
+    private String[] myColumnNames = { "Server Name",
+                                      "Map",
+                                      "Players" };
+
+    private int[][] myTest = { { 3, 3, 3 }, { 4, 4, 4 }, { 5, 5, 5 } };
+
     @Override
     public int getColumnCount () {
         return myColumnNames.length;
@@ -24,9 +28,9 @@ public class ServerBrowserTableAdapter extends AbstractTableModel {
     public int getRowCount () {
         return myTest.length;
     }
-    
+
     @Override
-    public String getColumnName(int col) {
+    public String getColumnName (int col) {
         return myColumnNames[col];
     }
 
--- src/vooga/rts/networking/client/GUI/ServerBrowserView.java
+++ src/vooga/rts/networking/client/GUI/ServerBrowserView.java
@@ -3,53 +3,70 @@ package vooga.rts.networking.client.GUI;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+
 /**
  * View for the server browser.
+ * 
  * @author David Winegar
- *
+ * 
  */
-public class ServerBrowserView extends JComponent {
+public class ServerBrowserView extends JPanel {
 
+    private static final long serialVersionUID = 5915312995153741263L;
     JTable myTable;
-    
+
+    /**
+     * Initialize view.
+     * 
+     * @param adapter adapter for server browser table
+     * @param gameName name of the game
+     */
     public ServerBrowserView (ServerBrowserTableAdapter adapter, String gameName) {
+        setLayout(new BorderLayout(0, 0));
         myTable = new JTable(adapter);
         myTable.setPreferredScrollableViewportSize(new Dimension(500, 70));
         myTable.setFillsViewportHeight(true);
-        JScrollPane pane = new JScrollPane();
-        pane.add(myTable);
-        BorderLayout layout = new BorderLayout();
-        setLayout(layout);
+        JScrollPane pane = new JScrollPane(myTable);
+
         add(pane, BorderLayout.CENTER);
         add(createTopPanel(gameName), BorderLayout.NORTH);
-        
-        setOpaque(true);
-        setVisible(true);
     }
-    
+
+    /**
+     * Creates the top panel, containing the JButton to host a game and the JLabel containing the
+     * game name.
+     */
     private JPanel createTopPanel (String gameName) {
         JPanel panel = new JPanel();
         BorderLayout layout = new BorderLayout();
         panel.setLayout(layout);
-        
+
         JPanel innerPanel = new JPanel();
         FlowLayout innerLayout = new FlowLayout();
         innerPanel.setLayout(innerLayout);
         innerPanel.add(new JLabel(gameName));
-        
+
         panel.add(innerPanel, BorderLayout.CENTER);
-        
+
         JButton hostGame = new JButton("Host Game");
+        hostGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed (ActionEvent arg0) {
+                // TODO connect with switching view
+            }
+        });
+
         panel.add(hostGame, BorderLayout.WEST);
-        
+
         return panel;
     }
-    
+
 }
