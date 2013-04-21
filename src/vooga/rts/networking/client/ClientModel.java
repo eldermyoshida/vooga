b9169ce312e714896ac86eb11733441610f671d7
David Winegar <david.s.winegar@gmail.com>
2013-04-21 18:48:21 -0400

    Changed server browser model to client model, edited client

--- src/vooga/rts/networking/client/ClientModel.java
+++ src/vooga/rts/networking/client/ClientModel.java
@@ -2,23 +2,30 @@ package vooga.rts.networking.client;
 
 import vooga.rts.networking.communications.Message;
 import vooga.rts.networking.communications.clientmessages.InitialConnectionMessage;
+import vooga.rts.networking.communications.servermessages.ServerInfoMessage;
 
-public class ServerBrowserModel implements IMessageReceiver {
+public class ClientModel implements IMessageReceiver, IClientModel {
 
     private IClient myClient = new Client(this); 
     
-    public ServerBrowserModel () {
-        // TODO get info from user
+    public ClientModel () {
+        // TODO get info from arcade
         Message initialConnection = new InitialConnectionMessage ("game", "user");
         myClient.sendData(initialConnection);
+        
+        
     }
 
     @Override
     public void getMessage (Message message) {
+        if (message instanceof ServerInfoMessage) {
+            ((ServerInfoMessage) message).affectClient(this);
+        }
+    }
+
+    @Override
+    public void closeConnection () {
         
     }
-    
-    
-    
     
 }
