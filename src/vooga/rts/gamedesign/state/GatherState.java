6695f2586e3e786303e60a3cbb84feadad9e77f6
Ryan Fishel <fishel.ryan@gmail.com>
2013-04-21 19:48:28 -0400

    javadocs for new state

--- src/vooga/rts/gamedesign/state/GatherState.java
+++ src/vooga/rts/gamedesign/state/GatherState.java
@@ -1,5 +1,12 @@
 package vooga.rts.gamedesign.state;
 
+/**
+ * This enum represents all the possible states of gathering which are waiting
+ * and gathering.
+ * 
+ * @author Ryan Fishel
+ *
+ */
 public enum GatherState {
 	GATHERING, WAITING;
 }
