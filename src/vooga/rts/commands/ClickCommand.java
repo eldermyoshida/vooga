a9bf36006cc508a692cf885184121d8cb8fb3ac8
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-04-22 03:17:11 -0400

    Distinguished between Click and Position Command

--- src/vooga/rts/commands/ClickCommand.java
+++ src/vooga/rts/commands/ClickCommand.java
@@ -3,31 +3,17 @@ package vooga.rts.commands;
 import util.input.PositionObject;
 import vooga.rts.util.Location;
 
-
 /**
- * This class and the position command are the same thing, so this one can be deleted to avoid confusion
- * @author Challen Herzberg-Brovold
+ * A ClickCommand is different from a Command in that in addition the name of 
+ * the action (right or left click) it also holds the position of the click
  * 
+ * @author Challen Herzberg-Brovold
+ *
  */
-public class ClickCommand extends Command {
+public class ClickCommand extends PositionCommand {
 
-    public Location myPosition;
-
-    /**
-     * 
-     * @param inputName
-     * @param position the position of the click
-     */
     public ClickCommand (String inputName, PositionObject position) {
-        super(inputName);
-        myPosition = new Location(position.getX(), position.getY());
+        super(inputName, position);
     }
-
-    /**
-     * 
-     * @return the position of the click.
-     */
-    public Location getPosition () {
-        return myPosition;
-    }
+    
 }
