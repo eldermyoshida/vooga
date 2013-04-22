377ce03f3f6efb24cb97a49435e8125f88216175
Eric Wu <eric.wu@duke.edu>
2013-04-21 23:52:38 -0400

    created GameMenu

--- src/vooga/rts/gui/menus/GameMenu.java
+++ src/vooga/rts/gui/menus/GameMenu.java
@@ -1,5 +1,6 @@
 package vooga.rts.gui.menus;
 
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -7,11 +8,13 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import vooga.rts.gui.Menu;
 
+
 public class GameMenu extends Menu {
 
-    private static final String DEFAULT_BG_LOCATION = "../../resources/images/gamemenu/menu_bg.png";
+    private static final String DEFAULT_BG_LOCATION = "src/vooga/rts/resources/images/gamemenu/menu_bg.png";
     private BufferedImage myBGImage;
-    public GameMenu() {
+
+    public GameMenu () {
         File imgFile = new File(DEFAULT_BG_LOCATION);
         try {
             myBGImage = ImageIO.read(imgFile);
@@ -20,5 +23,12 @@ public class GameMenu extends Menu {
             e.printStackTrace();
         }
         setImage(myBGImage);
+    }
+
+    @Override
+    protected void paintBG (Graphics2D pen) {
+        int x = 0;
+        int y = (int) pen.getDeviceConfiguration().getBounds().getHeight() - myBGImage.getHeight();
+        pen.drawImage(myImage, x, y, null);
     }
 }
