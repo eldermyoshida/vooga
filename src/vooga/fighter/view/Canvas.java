dc5f81e30ecef154f7002c34bc180f8edc3553aa
unknown <wam17@duke.edu>
2013-04-23 18:48:29 -0400

    GreyScale option available in Pixmap!  To activate, uncomment 'setImageToGreyScale();' in the method setImage.  Will clean up later.

--- src/vooga/fighter/view/Canvas.java
+++ src/vooga/fighter/view/Canvas.java
@@ -10,7 +10,6 @@ import javax.swing.JComponent;
 import vooga.fighter.controller.ViewDataSource;
 import vooga.fighter.view.CanvasLayout;
 
-
 /**
  * Creates an area of the screen in which the game will be drawn that supports:
  * <UL>
@@ -23,88 +22,96 @@ import vooga.fighter.view.CanvasLayout;
  * @author Wayne You
  */
 public class Canvas extends JComponent {
-    // default serialization ID
-    private static final long serialVersionUID = 1L;
+	// default serialization ID
+	private static final long serialVersionUID = 1L;
 
-    // game to be animated
-    private ViewDataSource myViewDataSource;
-    // current layout of the game
-    private CanvasLayout myLayout = null;
+	// game to be animated
+	private ViewDataSource myViewDataSource;
+	// current layout of the game
+	private CanvasLayout myLayout = null;
 
-    /**
-     * Create a panel so that it knows its size
-     */
-    public Canvas (Dimension size) {
-        // set size (a bit of a pain)
-        setPreferredSize(size);
-        setSize(size);
-        // prepare to receive input
-        setFocusable(true);
-        requestFocus();
-    }
-    
-    /**
-     * Sets the data source from the controller.
-     * @param data
-     */
-    public void setViewDataSource (ViewDataSource data) {
-        myViewDataSource = data;
-    }
-    
-    /**
-     * Sets up the layout of the view. Null implies no layout.
-     * @param layout
-     */
-    public void setLayout (CanvasLayout layout) {
-        myLayout = layout;
-    }
-    
-    /**
-     * Calls java's repaint method.
-     */
-    public void paint() {
-        repaint();
-    }
+	/**
+	 * Create a panel so that it knows its size
+	 */
+	public Canvas(Dimension size) {
+		// set size (a bit of a pain)
+		setPreferredSize(size);
+		setSize(size);
+		// prepare to receive input
+		setFocusable(true);
+		requestFocus();
+	}
 
-    /**
-     * Paint the contents of the canvas.
-     * 
-     * Never called by you directly, instead called by Java runtime
-     * when area of screen covered by this container needs to be
-     * displayed (i.e., creation, uncovering, change in status)
-     * 
-     * @param pen used to paint shape on the screen
-     */
-    @Override
-    public void paintComponent (Graphics pen) {
-        pen.setColor(Color.WHITE);
-        pen.fillRect(0, 0, getSize().width, getSize().height);
-        // If there is no defined layout, simply paint things at the locations they are given.
-        if (myLayout == null) {
-            for (int i = 0; i < myViewDataSource.ObjectNumber(); i++) {
-                paintPaintable(pen, i);
-            }
-        }
-        else {
-            myLayout.paintComponents((Graphics2D)pen, myViewDataSource, this.getSize());
-        }
-    }
-    
-    private void paintPaintable (Graphics pen, int i) {
-    	
-    	if (myViewDataSource.getImageEffects(i).size() == 0){
-    	
-    	myViewDataSource.getPaintable(i).paint((Graphics2D) pen,
-                myViewDataSource.getLocation(i),
-                myViewDataSource.getSize(i));
-    	}
-    	
-    	else if (myViewDataSource.getImageEffects(i).get(0) == -1){
-        	
-        	myViewDataSource.getPaintable(i).paintReverse((Graphics2D) pen,
-                    myViewDataSource.getLocation(i),
-                    myViewDataSource.getSize(i));
-        	}
-    }
+	/**
+	 * Sets the data source from the controller.
+	 * 
+	 * @param data
+	 */
+	public void setViewDataSource(ViewDataSource data) {
+		myViewDataSource = data;
+	}
+
+	/**
+	 * Sets up the layout of the view. Null implies no layout.
+	 * 
+	 * @param layout
+	 */
+	public void setLayout(CanvasLayout layout) {
+		myLayout = layout;
+	}
+
+	/**
+	 * Calls java's repaint method.
+	 */
+	public void paint() {
+		repaint();
+	}
+
+	/**
+	 * Paint the contents of the canvas.
+	 * 
+	 * Never called by you directly, instead called by Java runtime when area of
+	 * screen covered by this container needs to be displayed (i.e., creation,
+	 * uncovering, change in status)
+	 * 
+	 * @param pen
+	 *            used to paint shape on the screen
+	 */
+	@Override
+	public void paintComponent(Graphics pen) {
+		pen.setColor(Color.WHITE);
+		pen.fillRect(0, 0, getSize().width, getSize().height);
+		// If there is no defined layout, simply paint things at the locations
+		// they are given.
+		if (myLayout == null) {
+			for (int i = 0; i < myViewDataSource.ObjectNumber(); i++) {
+				// paintPaintable(pen, i);
+				myViewDataSource.getPaintable(i).paint((Graphics2D) pen,
+						myViewDataSource.getLocation(i),
+						myViewDataSource.getSize(i));
+
+			}
+		} else {
+			myLayout.paintComponents((Graphics2D) pen, myViewDataSource,
+					this.getSize());
+		}
+	}
+
+	private void paintPaintable(Graphics pen, int i) {
+
+		if (myViewDataSource.getImageEffects(i).size() == 0) {
+
+			myViewDataSource.getPaintable(i).paint((Graphics2D) pen,
+					myViewDataSource.getLocation(i),
+					myViewDataSource.getSize(i));
+		}
+
+		else if (myViewDataSource.getImageEffects(i).get(0) == -1) {
+
+			myViewDataSource.getPaintable(i).paintReverse((Graphics2D) pen,
+					myViewDataSource.getLocation(i),
+					myViewDataSource.getSize(i));
+		}
+	}
 
 }
