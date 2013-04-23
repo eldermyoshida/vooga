e5480419faf3080985ee4b43dc10ef8adb1ab61a
Jay Wang <jayduke15@gmail.com>
2013-04-22 21:43:26 -0400

    Added documentation for all the interfaces.

--- src/vooga/scroller/sprites/interfaces/ISprite.java
+++ src/vooga/scroller/sprites/interfaces/ISprite.java
@@ -1,10 +1,92 @@
 package vooga.scroller.sprites.interfaces;
 
+import util.Vector;
+
+
+/**
+ * The super-interface that all other interfaces extend. This just provides all the interfaces
+ * with access to basic sprite methods that returns basic location about the sprite. Now, the
+ * visit()
+ * methods have access to all of these methods on the interfaces they operate on.
+ * 
+ * @author Jay Wang
+ * 
+ */
 public interface ISprite {
 
+    /**
+     * Returns the bottom pixel of the Sprite.
+     * 
+     * @return double
+     */
     public double getBottom ();
+
+    /**
+     * Returns the top pixel of the Sprite.
+     * 
+     * @return double
+     */
     public double getTop ();
+
+    /**
+     * Returns the left pixel of the Sprite.
+     * 
+     * @return double
+     */
     public double getLeft ();
+
+    /**
+     * Returns the right pixel of the Sprite.
+     * 
+     * @return double
+     */
     public double getRight ();
+
+    /**
+     * Returns the location of the Sprite's X location
+     * 
+     * @return double
+     */
+    public double getX ();
+
+    /**
+     * Returns the location of the Sprite's Y location
+     * 
+     * @return double
+     */
+    public double getY ();
+
+    /**
+     * Returns the size of the Sprite's width in pixels
+     * 
+     * @return double
+     */
+    public double getWidth ();
+
+    /**
+     * Returns the size of the Sprite's height in pixels
+     * 
+     * @return double
+     */
+    public double getHeight ();
+
+    /**
+     * Sets the center of the Sprite to Location(x,y)
+     */
+    public void setCenter (double x, double y);
+
+    /**
+     * Adds the Vector r to the current Velocity vector of the Sprite.
+     * 
+     * @param Vector
+     */
+    public void addVector (Vector r);
+
+    /**
+     * Returns the Velocity vector of the Sprite.
+     * 
+     * @return Vector
+     */
+    public Vector getVelocity ();
 
 }
