6e721a8623981928342dedeb4b1bae723a66d487
Matt <mmparides91@gmail.com>
2013-04-22 22:58:19 -0400

    added removeState to GameObjects, finished placement method for editor

--- src/vooga/fighter/controller/MapEditorController.java
+++ src/vooga/fighter/controller/MapEditorController.java
@@ -8,10 +8,12 @@ import vooga.fighter.input.AlertObject;
 import vooga.fighter.input.InputClassTarget;
 import vooga.fighter.input.InputMethodTarget;
 import vooga.fighter.input.PositionObject;
+import vooga.fighter.model.CollisionManager;
 import vooga.fighter.model.LevelMode;
 import vooga.fighter.model.MapEditorMode;
 import vooga.fighter.model.Mode;
 import vooga.fighter.model.objects.CharacterObject;
+import vooga.fighter.model.objects.EnvironmentObject;
 import vooga.fighter.model.objects.MapObject;
 import vooga.fighter.view.Canvas;
 
@@ -31,9 +33,15 @@ public class MapEditorController extends Controller{
         setInput(manager.getInput());
         getInput().replaceMappingResourcePath(INPUT_PATHWAY);
         getInput().addListenerTo(this);
+<<<<<<< HEAD
         GameLoopInfo gameLoopInfo = new GameLoopInfo((LevelMode) getMode());
         setLoopInfo(gameLoopInfo);
         gameinfo.setGameLoopInfo(gameLoopInfo);
+=======
+     //   GameLoopInfo gameLoopInfo = new GameLoopInfo((MapEditorMode) getMode());
+    //    setLoopInfo(gameLoopInfo);
+     //   gameinfo.setGameLoopInfo(gameLoopInfo);
+>>>>>>> added removeState to GameObjects, finished placement method for editor
 	    }
 	    
 	    public Controller getController(String name, Canvas frame, ControllerDelegate manager, GameInfo gameinfo) {
@@ -46,17 +54,17 @@ public class MapEditorController extends Controller{
 	    	getManager().notifyEndCondition(endCondition);
 	    }
 	    
-
+	    public MapEditorMode getMode(){
+	    	return (MapEditorMode) super.getMode();
+	    }
 	    
 	    public void loadMode() {
-	       // List<Integer> characterNames = myGameInfo.getCharacters();
-	        String mapID = getGameInfo().getMapName();
-	   //     Mode temp = new LevelMode(this, mapID);
-	   //     setMode(temp);
+	        MapEditorMode temp = new MapEditorMode(new CollisionManager());
+	        super.setMode((Mode) temp);
 	    }
 	    
 	    public void initializeMode() {
-	    	
+	    	loadMap(getGameInfo().getMapName());
 	    }
 	    
 	    public Controller getController() {
@@ -67,21 +75,16 @@ public class MapEditorController extends Controller{
 	    	myEditTarget = map;
 	    }
 
-	    /*
-	    @Override
-	    public Controller getController (ControllerDelegate delegate, GameInfo gameinfo) {
-	        return new MapEditorController(super.getName(), super.getView(),
-	                                   delegate, gameinfo);
+	    /**
+	     * Loads the environment objects for a map using the ObjectLoader.
+	     */
+	    public void loadMap(String mapName) {
+	    	getMode().setMap(new MapObject(mapName));
+	        List<EnvironmentObject> mapObjects = getMode().getMap().getEnviroObjects();
+	        for (EnvironmentObject object : mapObjects) {
+	        	getMode().addObject(object);
+	        }
 	    }
-	    
-
-	    @Override
-	    protected Input makeInput () {
-	        Input input = new Input(INPUT_PATHWAY, super.getView());
-	        input.addListenerTo(this);
-	    	return input;
-	    }
-	    */
 	    
 	    @InputMethodTarget(name = "load")
 	    public void loadMap (AlertObject alObj)  {
