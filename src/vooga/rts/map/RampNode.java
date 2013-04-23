66db330d20e78e0cc380d8da504d4a60573c59c1
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-04-23 01:48:25 -0400

    Removed old stuff on the RampNode

--- src/vooga/rts/map/RampNode.java
+++ src/vooga/rts/map/RampNode.java
@@ -1,20 +1,18 @@
 package vooga.rts.map;
 
-import vooga.rts.util.Location;
 /**
- * Ramp nodes connect to every node touching them. This will be used for more 
+ * Ramp nodes connect to every node touching them. This will be used for more
  * advanced maps. The fact that its a ramp node is specified by its height of -1
  * 
  * @author Challen Herzberg-Brovold
- *
+ * 
  */
 public class RampNode extends Node {
-    
-    
-    public RampNode (int x, int y, Location center) {
-        super(x, y, -1, center);
+
+    public RampNode (int x, int y) {
+        super(x, y, -1);
     }
-    
+
     /**
      * Connects to any node it is touching.
      */
