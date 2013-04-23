3bd7def1d13b6c6e5c7335d3acd98159cc2911ae
David Winegar <david.s.winegar@gmail.com>
2013-04-23 02:01:56 -0400

    Edited clientModel and expandedLobbyInfo

--- src/vooga/rts/networking/client/ClientModel.java
+++ src/vooga/rts/networking/client/ClientModel.java
@@ -66,6 +66,23 @@ public class ClientModel implements IMessageReceiver, IClientModel, IModel {
     private JPanel getPanel () {
         return myContainerPanel;
     }
+    
+    /**
+     * testing
+     */
+    public static void main (String[] args) {
+        ClientModel model =
+                new ClientModel("Test Game", "User 1", new String[] { "protoss", "zerg" },
+                                new String[] { "map1", "map2" },
+                                new Integer[][] { { 2, 3, 4 }, { 2, 3, 4, 5, 6 } });
+
+        JFrame frame = new JFrame();
+        frame.add(model.getPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(600, 500));
+        frame.setVisible(true);
+        frame.pack();
+    }
 
     @Override
     public void getMessage (Message message) {
@@ -157,23 +174,6 @@ public class ClientModel implements IMessageReceiver, IClientModel, IModel {
         myClient.sendData(new StartGameMessage());
     }
 
-    /**
-     * testing
-     */
-    public static void main (String[] args) {
-        ClientModel model =
-                new ClientModel("Test Game", "User 1", new String[] { "protoss", "zerg" },
-                                new String[] { "map1", "map2" },
-                                new Integer[][] { { 2, 3, 4 }, { 2, 3, 4, 5, 6 } });
-
-        JFrame frame = new JFrame();
-        frame.add(model.getPanel());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(600, 500));
-        frame.setVisible(true);
-        frame.pack();
-    }
-
     @Override
     public void addLobbies (LobbyInfo[] lobbies) {
         myServerBrowserView.addLobbies(lobbies);
@@ -181,24 +181,21 @@ public class ClientModel implements IMessageReceiver, IClientModel, IModel {
 
     @Override
     public void updateFaction (String faction, int position) {
-        
+        myLobbyInfo.getPlayerAtPosition(position).setFaction(faction);
     }
 
     @Override
     public void updateTeam (int team, int position) {
-        // TODO Auto-generated method stub
-
+        myLobbyInfo.getPlayerAtPosition(position).setTeam(team);
     }
 
     @Override
     public void switchToLobbyView (ExpandedLobbyInfo lobbyInfo) {
-        // TODO Auto-generated method stub
-        
+        myLobbyInfo = lobbyInfo;
     }
 
     @Override
     public void updateLobbyView (ExpandedLobbyInfo lobbyInfo) {
-        // TODO Auto-generated method stub
-        
+        myLobbyInfo = lobbyInfo;
     }
 }
