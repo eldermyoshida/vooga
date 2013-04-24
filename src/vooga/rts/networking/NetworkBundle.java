e41f932b50db4932e43c007d2e67756fd6551c84
Henrique Moraes <hrm8@duke.edu>
2013-04-23 20:23:39 -0400

    JD Bundle

--- src/vooga/rts/networking/NetworkBundle.java
+++ src/vooga/rts/networking/NetworkBundle.java
@@ -2,6 +2,17 @@ package vooga.rts.networking;
 
 import java.util.ResourceBundle;
 
+/**
+ * Class used to manage resources of the networking package
+ * By default, the resourcebundle seeks an english.properties file and can be
+ * accessed statically to preserve lines of code
+ * 
+ * However, the user can still get a different resourcebundle if he sets the language
+ * and/or path
+ * 
+ * @author Henrique Moraes
+ *
+ */
 public class NetworkBundle {
 	private static String PROPERTIES = ".properties";
 	private static String LANGUAGE = "english";
@@ -10,13 +21,32 @@ public class NetworkBundle {
 	
 	private ResourceBundle myBundle;
 	private String myLanguage;
+	private String myRelativePath = RESOURCE_PACKAGE;
 	
+	/**
+	 * 
+	 * @return ResourceBundle
+	 */
 	public ResourceBundle getBundle() {
 		return myBundle;
 	}
 	
+	/**
+	 * Sets the language used by this resourcebundle according to its filename 
+	 * @param language Name of the file in which resources are located
+	 */
 	public void setLanguage(String language) {
 		myLanguage = language;
-		myBundle = ResourceBundle.getBundle(RESOURCE_PACKAGE+myLanguage+PROPERTIES);
+		myBundle = ResourceBundle.getBundle(myRelativePath+myLanguage+PROPERTIES);
+	}
+	
+	/**
+	 * 
+	 * @param path The relative path to the desired file
+	 */
+	public void setRelativePath(String path) {
+		if (path.charAt(path.length()-1) != '.')
+			path = path+'.';
+		myRelativePath = path;
 	}
 }
