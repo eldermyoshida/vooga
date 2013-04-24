bd23f0cc0caae10c955ed2b0492e96120fe14341
unknown <wam17@duke.edu>
2013-04-24 00:15:58 -0400

    cleaned up Pixmap.

--- src/vooga/fighter/view/PaletteSwap.java
+++ src/vooga/fighter/view/PaletteSwap.java
@@ -1,39 +1,29 @@
 package vooga.fighter.view;
 
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.RescaleOp;
 
+import sun.awt.image.ToolkitImage;
 import util.Pixmap;
+import vooga.fighter.util.Paintable;
 
 public class PaletteSwap {
-	// palette swapping is a common practice in fighting games
-	// palette swapping can be used to create new character models by recycling
-	// old ones
-	// palette swapping can also be used to help players distinguish each other
-	// when they both
-	// select the same character
-	private Pixmap ObjectImage;
 
-	public PaletteSwap() {
+	public PaletteSwap(Image i) {
 
 	}
 
-	public static void applyGreyscale(Image colorImage, int width, int height) {
-		BufferedImage bufferedImage = new BufferedImage(width, height,
-				BufferedImage.TYPE_BYTE_GRAY);
-		//Graphics g = image.getGraphics();
-		//g.drawImage(colorImage, 0, 0, null);
-		//g.dispose();
-		RescaleOp op = new RescaleOp(.9f, 0, null);
-	    bufferedImage = op.filter(bufferedImage, null);
-	}
-	
-	public void applyDarken(Image colorImage, int width, int height) throws Exception {
-	    BufferedImage bufferedImage = new BufferedImage(width, height,
-	        BufferedImage.TYPE_BYTE_INDEXED);
-
-	    RescaleOp op = new RescaleOp(.9f, 0, null);
-	    bufferedImage = op.filter(bufferedImage, null);
+	public void setImageToGreyScale(Paintable paintable) {
+		BufferedImage buffered = ((ToolkitImage) paintable).getBufferedImage();
+		BufferedImage temp = new BufferedImage(buffered.getWidth(),
+				buffered.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+		temp.getScaledInstance(buffered.getWidth(), buffered.getHeight(), 0);
+		Graphics g = temp.getGraphics();
+		g.drawImage(buffered, 0, 0, null);
+		buffered = temp;
+		g.dispose();
+		paintable = (Image) buffered;
 	}
 }
