e9b0ac9a90188df24d1a29da982b7593edfa58a2
Eric Wu <eric.wu@duke.edu>
2013-04-24 00:02:21 -0400

    added hover description functionality

--- src/vooga/rts/gui/buttons/ActionButton.java
+++ src/vooga/rts/gui/buttons/ActionButton.java
@@ -9,19 +9,35 @@ import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import vooga.rts.gui.Button;
+import vooga.rts.util.Information;
 import vooga.rts.util.Location;
 
 
 public class ActionButton extends ImageButton {
+    
+    private Information myInfo;
 
-    public ActionButton (BufferedImage image, Dimension size, Location pos) {
+    public ActionButton (BufferedImage image, Dimension size, Location pos, Information i) {
         super(image, size, pos);
-        // TODO Auto-generated constructor stub
+        myInfo = i;
     }
     
-    public ActionButton (String image, Dimension size, Location pos) {
+    public ActionButton (String image, Dimension size, Location pos, Information i) {
         super(image, size, pos);
-        // TODO Auto-generated constructor stub
+        myInfo = i;
+    }
+    
+    @Override
+    public void paint (Graphics2D pen) {
+        super.paint(pen);
+        if (isFocused) {
+            pen.drawString(myInfo.getName(), (int) myPos.getX(), (int) myPos.getY());
+        }
+    }
+    
+    @Override
+    public void processHover() {
+        
     }
 
 
