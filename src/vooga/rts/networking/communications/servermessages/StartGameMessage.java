00812441b1c83fd452fe0aab1d490fab1c2e4ada
David Winegar <david.s.winegar@gmail.com>
2013-04-24 04:08:59 -0400

    Edited merge

--- src/vooga/rts/networking/communications/servermessages/StartGameMessage.java
+++ src/vooga/rts/networking/communications/servermessages/StartGameMessage.java
@@ -3,6 +3,13 @@ package vooga.rts.networking.communications.servermessages;
 import vooga.rts.networking.client.IClientModel;
 import vooga.rts.networking.communications.Message;
 
+
+/**
+ * Starts the game on each client.
+ * 
+ * @author David Winegar
+ * 
+ */
 public class StartGameMessage extends Message implements ServerInfoMessage {
 
     private static final long serialVersionUID = -140612656634180594L;
