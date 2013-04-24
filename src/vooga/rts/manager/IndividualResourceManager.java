95b36ab49321935eb3063cfabb6a0a5ad6d0b6d1
Jonno Schmidt <jonno.schmidt@duke.edu>
2013-04-24 11:46:19 -0400

    Added Individual Resource Mananger

--- src/vooga/rts/manager/IndividualResourceManager.java
+++ src/vooga/rts/manager/IndividualResourceManager.java
@@ -3,6 +3,7 @@ package vooga.rts.manager;
 import java.util.HashMap;
 import java.util.Map;
 
+
 /**
  * This class stores all the resources that a particular player has.
  * It provides methods to check if the player has enough resources to
@@ -13,21 +14,66 @@ import java.util.Map;
  * 
  */
 public class IndividualResourceManager {
-    
+
     private Map<String, Integer> myResources;
-    
-    public IndividualResourceManager() {
-        myResources = new HashMap<String, Integer>();        
+
+    /**
+     * Creates an individual resource manager.
+     */
+    public IndividualResourceManager () {
+        myResources = new HashMap<String, Integer>();
     }
-    
-    public int getAmount(String resource) {
-        if (myResources.containsKey(resource)){
-            return myResources.get(resource);
+
+    /**
+     * Returns how much of a type of resource the player has.
+     * 
+     * @param resource The resource type.
+     * @return the amount of resources
+     */
+    public int getAmount (String resource) {
+        if (myResources.containsKey(resource)) {
+            return myResources.get(resource.toLowerCase());
         }
         return 0;
     }
-    
+
+    /**
+     * Sets the resource manager map.
+     * 
+     * @param resources
+     */
     public void setResources (HashMap<String, Integer> resources) {
-    	myResources = resources;
+        myResources = resources;
     }
+
+    /**
+     * Sets the amount of resources of a type that the player has.
+     * 
+     * @param type The type of resource
+     * @param value The amount of resources
+     */
+    public void setAmount (String type, int value) {
+        myResources.put(type.toLowerCase(), value);
+    }
+
+    /**
+     * Charges the resource manager a certain amount of a type of resource.
+     * 
+     * @param type The type of resource
+     * @param amount The cost of this type of resource
+     */
+    public void charge (String type, int amount) {
+        setAmount(type, getAmount(type) - amount);
+    }
+    
+    /**
+     * Provides a certain amount of a type of resources to the player.
+     * 
+     * @param type The type of resource
+     * @param amount The amount of this type of resource earned
+     */
+    public void earn (String type, int amount) {
+        setAmount(type, getAmount(type) - amount);
+    }
+
 }
