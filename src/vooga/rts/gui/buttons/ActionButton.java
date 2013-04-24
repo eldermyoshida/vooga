aeaf93ed4c133e03c27305fa5c523e28f8496f7a
Eric Wu <eric.wu@duke.edu>
2013-04-23 23:54:45 -0400

    ActionButton is a reflection of the ImageButton class, used by ActionsSubMenu

--- src/vooga/rts/gui/buttons/ActionButton.java
+++ src/vooga/rts/gui/buttons/ActionButton.java
@@ -7,60 +7,22 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
 import vooga.rts.gui.Button;
 import vooga.rts.util.Location;
 
 
-public class ActionButton extends Button {
+public class ActionButton extends ImageButton {
 
-    private String myText;
-    private Color myBGColor;
-    private Color myTextColor;
-    private int fontSize;
-    private int myXPadding = 5;
-    private int myYPadding = 10;
-    private int myID;
-
-    public ActionButton (String text, int i, Color d, int f, Location pos) {
-        super(null, null, pos);
-        myText = text;
-        myTextColor = d;
-        fontSize = f;
-        myID = i;
-    }
-
-    @Override
-    public void update (double elapsedTime) {
-        setChanged();
-        notifyObservers();
-    }
-
-    @Override
-    public void paint (Graphics2D pen) {
-        pen.setFont(new Font("Arial", Font.BOLD, fontSize));
-        int width = pen.getFontMetrics().stringWidth(myText);
-        int height = pen.getFontMetrics().getHeight();
-        
-        mySize = new Dimension(width, height);
-        pen.setColor(myTextColor);
-        pen.drawString(myText, (int) myPos.getX(), (int) myPos.getY() + height);
-        // pen.drawRect((int) myPos.getX(), (int) myPos.getY()-height, width, height);
-
+    public ActionButton (BufferedImage image, Dimension size, Location pos) {
+        super(image, size, pos);
+        // TODO Auto-generated constructor stub
     }
     
-    public int getID () {
-        return myID;
+    public ActionButton (String image, Dimension size, Location pos) {
+        super(image, size, pos);
+        // TODO Auto-generated constructor stub
     }
 
-    @Override
-    public void processClick () {
-        update(0);
-    }
-
-    @Override
-    public void processHover () {
-        // TODO Auto-generated method stub
-
-    }
 
 }
