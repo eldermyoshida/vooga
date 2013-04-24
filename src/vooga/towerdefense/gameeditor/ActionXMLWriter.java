b86a07ec78e8caa99bc6fe4ef4a711c3a117c806
Erick Gonzalez <erick.gonzal3z@gmail.com>
2013-04-23 20:13:50 -0400

    Checkstyled and added documentation to ActionXMLWriter.java and MapLoader.java

--- src/vooga/towerdefense/gameeditor/ActionXMLWriter.java
+++ src/vooga/towerdefense/gameeditor/ActionXMLWriter.java
@@ -5,74 +5,125 @@ import java.util.List;
 import org.w3c.dom.Element;
 import util.XMLTool;
 
+
 /**
+ * This class is responsible for reading in a data string containing information 
+ * about actions, their parameters, and their sub-actions. The class reads this
+ * string, and then can return a root Element for an xml file.
+ * 
+ * An example of the data string can be shown below:
+ * 
+ * Action1 param1 param2
+ *      Action3 param1 param2 param3
+ *              Action4 param1
+ *      Action5 param1
+ * Action2 param1 param2 param3 param4
+ * 
+ * This data string is then converted to the following XML.
+ * 
+ * <actions>
+ *      <action1>
+ *              <parameter>param1</parameter>
+ *              <parameter>param2</parameter>
+ *              <action3>
+ *                      <parameter>param1</parameter>
+ *                      <parameter>param2</parameter>
+ *                      <parameter>param3</parameter>
+ *                      <action4>
+ *                              <parameter>param1</parameter>
+ *                      </action4>
+ *              </action3>
+ *              <action5>
+ *                      <parameter>param1</parameter>
+ *              </action5>      
+ *      </action1>
+ *      <action2>
+ *              <parameter>param1</parameter>
+ *              <parameter>param2</parameter>
+ *              <parameter>param3</parameter>
+ *              <parameter>param4</parameter>
+ *      </action2>
+ * </actions>
  * 
  * @author Erick Gonzalez
  */
 public class ActionXMLWriter {
     private static final String PARAM_TAG = "parameter";
-    
-    private int index;
+    private static final char TAB = '\t';
+    private static final String WHITESPACE_REGEX = "\\s+";
+
+    private int myIndex;
     private XMLTool myXMLTool;
-    
-    public ActionXMLWriter(XMLTool xmlTool) {
-        index = 0;
+
+    /**
+     * 
+     * @param xmlTool an xml tool containing a document being written to
+     */
+    public ActionXMLWriter (XMLTool xmlTool) {
+        myIndex = 0;
         myXMLTool = xmlTool;
     }
-    
-    public Element parse(Element rootElement, String dataString) {
+
+    /**
+     * 
+     * @param rootElement the root XML element
+     * @param dataString the string of actions
+     * @return the rootElement, now containing all its appropriate subElements.
+     */
+    public Element parse (Element rootElement, String dataString) {
         String[] dataArray = dataString.split("\n");
-        
-        while (index < dataArray.length) {
+
+        while (myIndex < dataArray.length) {
             myXMLTool.addChildElement(rootElement, parse(dataArray, 0));
-        }        
+        }
         return rootElement;
     }
-    
-    private Element parse(String[] dataArray, int numTabs) {
-        String actionString = dataArray[index++];
+
+    private Element parse (String[] dataArray, int numTabs) {
+        String actionString = dataArray[myIndex++];
         String actionName = getActionName(actionString);
         List<String> params = getParams(actionString);
-        
+
         Element currentElement = myXMLTool.makeElement(actionName);
-        
+
         for (String param : params) {
-           Element parameterElement = myXMLTool.makeElement(PARAM_TAG, param);
-           myXMLTool.addChildElement(currentElement, parameterElement);
+            Element parameterElement = myXMLTool.makeElement(PARAM_TAG, param);
+            myXMLTool.addChildElement(currentElement, parameterElement);
         }
-        while (index < dataArray.length) {
-            String next = dataArray[index];
+        while (myIndex < dataArray.length) {
+            String next = dataArray[myIndex];
             if (countTabs(next) > numTabs) {
                 Element child = parse(dataArray, numTabs + 1);
                 myXMLTool.addChildElement(currentElement, child);
-            } else {
+            }
+            else {
                 break;
             }
         }
         return currentElement;
     }
-    
-    private String getActionName(String actionString) {
-        String[] actionSplitString = actionString.trim().split("\\s+");
+
+    private String getActionName (String actionString) {
+        String[] actionSplitString = actionString.trim().split(WHITESPACE_REGEX);
         return actionSplitString[0];
     }
-    
-    private List<String> getParams(String actionString) {
-        String[] actionSplitString = actionString.trim().split("\\s+");
+
+    private List<String> getParams (String actionString) {
+        String[] actionSplitString = actionString.trim().split(WHITESPACE_REGEX);
         List<String> params = new ArrayList<String>();
         for (int i = 1; i < actionSplitString.length; ++i) {
             params.add(actionSplitString[i]);
         }
         return params;
     }
- 
-    private int countTabs(String test) {
+
+    private int countTabs (String s) {
         int count = 0;
-        for (int i = 0; i < test.length(); ++i) {
-            if (test.charAt(i) == '\t') {
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == TAB) {
                 count++;
             }
         }
         return count;
-    }    
+    }
 }
