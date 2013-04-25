4655bb8dc274a57cc0ba6aac42aa4ff24391b6c9
Will Nance <wllnance@gmail.com>
2013-04-25 13:17:31 -0400

    sprite library

---
+++ src/games/scroller/arcadedemopackage/DemoLib.java
@@ -0,0 +1,25 @@
+package games.scroller.arcadedemopackage;
+
+import vooga.scroller.level_editor.library.EncapsulatedSpriteLibrary;
+import vooga.scroller.level_editor.library.ISpriteLibrary;
+import vooga.scroller.util.Sprite;
+
+/**
+ * implement types of Sprites that will be in you game in this file with the
+exception of the Sprite that extends Player
+ * @author Will Nance
+ *
+ */
+public class DemoLib extends EncapsulatedSpriteLibrary{
+
+    public DemoLib () {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Class<? extends Sprite>[] getSpritesClasses () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/games/scroller/arcadedemopackage/collisions/VisitMethods.java
@@ -0,0 +1,9 @@
+package games.scroller.arcadedemopackage.collisions;
+
+public class VisitMethods {
+
+    public VisitMethods () {
+        // TODO Auto-generated constructor stub
+    }
+
+}
