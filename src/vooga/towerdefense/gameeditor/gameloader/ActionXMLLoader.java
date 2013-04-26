9eb4cf3673993ffbdd04db1fda778330407e8145
Erick Gonzalez <erick.gonzal3z@gmail.com>
2013-04-26 17:26:10 -0400

    fixed some bugs in ActionXMLLoader

--- src/vooga/towerdefense/gameeditor/gameloader/ActionXMLLoader.java
+++ src/vooga/towerdefense/gameeditor/gameloader/ActionXMLLoader.java
@@ -11,20 +11,43 @@ import vooga.towerdefense.action.Action;
 import vooga.towerdefense.factories.actionfactories.ActionFactory;
 import vooga.towerdefense.gameElements.GameElement;
 
+/**
+ * This class is responsible for loading all Action objects.  
+ * 
+ * @author Erick Gonzalez
+ */
 public class ActionXMLLoader {
     private static final String ACTIONS_TAG = "Actions";
     private static final String PARAMETER_TAG = "parameter";
     
     private XMLTool myXMLTool;
-    
+   
+    /**
+     * 
+     * 
+     * @param xmlTool an xmlTool 
+     */
     public ActionXMLLoader(XMLTool xmlTool) {
         myXMLTool = xmlTool;
     }
     
+    /**
+     * This method loads all the actions described in XML that
+     * do not act upon a GameElement object.
+     * 
+     * @return loads a list of actions
+     */
     public List<Action> loadActions() {
         return loadActions(null);
     }
     
+    /**
+     * This method loads all the actions described in XML that
+     * act upon a GameElement object e.
+     * 
+     * @param e the game element object that an action acts on
+     * @return a list of actions acting on e
+     */
     public List<Action> loadActions(GameElement e) {
         List<ActionFactory> actionFactories = loadActionFactories();
         
@@ -36,18 +59,25 @@ public class ActionXMLLoader {
         return actions; 
     }
     
+    /**
+     * 
+     * @return a list of action factories
+     */
     public List<ActionFactory> loadActionFactories() {
         Element actionsElement = myXMLTool.getElement(ACTIONS_TAG);                
         Map<String, Element> subElements = myXMLTool.getChildrenElementMap(actionsElement);
-        List<ActionFactory> actions = new ArrayList<ActionFactory>();
+        
+        List<ActionFactory> actionFactories = new ArrayList<ActionFactory>();
         for (Element e : subElements.values()) {
-            actions.add(loadActionFactory(e));
+            actionFactories.add(loadActionFactory(e));
         }
-        return actions;
+        return actionFactories;
     }
     
     private ActionFactory loadActionFactory(Element actionElement) {
-        String actionName = myXMLTool.getContent(actionElement);
+        String actionName = myXMLTool.getTagName(actionElement);
+        System.out.println(actionName);
+        
         List<String> parameterStrings = new ArrayList<String>();
         Object[] parameterStringsArray = parameterStrings.toArray();
         
@@ -92,5 +122,14 @@ public class ActionXMLLoader {
     
     private String loadParameterString(Element parameterElement) {
         return myXMLTool.getContent(parameterElement);
+    }
+    
+    public static void main(String[] args) {
+        String actionsPath = System.getProperty("user.dir");
+        System.out.println(actionsPath);
+        XMLTool tool = new XMLTool("/actions.xml");
+        ActionXMLLoader loader = new ActionXMLLoader(tool);
+        List<ActionFactory> actionFactories = loader.loadActionFactories();
+        List<Action> action = loader.loadActions();
     }
 }
