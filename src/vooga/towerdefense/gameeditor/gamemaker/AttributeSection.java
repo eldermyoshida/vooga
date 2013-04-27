92d72ce31ddeec254bf5434cafd9a3a84e8fef40
Angelica <angeschwartz@gmail.com>
2013-04-27 18:06:02 -0400

    added annotations in wave factory

---
+++ src/vooga/towerdefense/gameeditor/gamemaker/AttributeSection.java
@@ -0,0 +1,138 @@
+package vooga.towerdefense.gameeditor.gamemaker;
+
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class AttributeSection extends SubEditorScreen {
+
+    /**
+     * default serialized id.
+     */
+    private static final long serialVersionUID = 1L;
+    private static final String NEW_TEXT = "New";
+    private static final String ATTRIBUTE_ADD_BUTTON_TEXT = "Add";
+    private static final String ATTRIBUTE_DELETE_BUTTON_TEXT = "Delete";
+    private JComboBox myAttributesBox;
+    private JTextField myAttributeValue;
+    private JTextArea myAttributesSelected;
+    private JButton myAddAttributeButton;
+    private JButton myDeleteAttributeButton;
+
+    public AttributeSection(String title, List<String> attributes, MouseAdapter mouseAdapter) {
+        super(title);
+        try {
+            makeAttributesSection(attributes, mouseAdapter);
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * helper method to make the attributes drop down box,
+     * button, and text field.
+     * 
+     * @throws ClassNotFoundException
+     */
+    private void makeAttributesSection (List<String> attributes, MouseAdapter mouseAdapter) throws ClassNotFoundException {
+        JPanel optionsSubPanel1 = new JPanel(new BorderLayout());
+        optionsSubPanel1.add(new JLabel(getTitle()), BorderLayout.NORTH);
+        myAttributesBox = new JComboBox();
+        attributes.add(NEW_TEXT);
+        for (String a : attributes) {
+            myAttributesBox.addItem(a);
+        }
+        optionsSubPanel1.add(myAttributesBox, BorderLayout.CENTER);
+        myAttributeValue = new JTextField();
+        optionsSubPanel1.add(myAttributeValue, BorderLayout.SOUTH);
+        add(optionsSubPanel1, BorderLayout.WEST);
+        myAttributesSelected = new JTextArea(TEXT_AREA_WIDTH, TEXT_AREA_HEIGHT);
+        add(new JScrollPane(myAttributesSelected), BorderLayout.CENTER);
+        JPanel optionsSubPanel2 = new JPanel(new BorderLayout());
+        myAddAttributeButton = new JButton(ATTRIBUTE_ADD_BUTTON_TEXT);
+        myAddAttributeButton.addMouseListener(mouseAdapter);
+        optionsSubPanel2.add(myAddAttributeButton, BorderLayout.NORTH);
+        myDeleteAttributeButton = new JButton(ATTRIBUTE_DELETE_BUTTON_TEXT);
+        myDeleteAttributeButton.addMouseListener(mouseAdapter);
+        optionsSubPanel2.add(myDeleteAttributeButton, BorderLayout.SOUTH);
+        JPanel eastSide = new JPanel(new BorderLayout());
+        eastSide.add(optionsSubPanel2, BorderLayout.NORTH);
+        add(eastSide, BorderLayout.EAST);
+    }
+    
+    @Override
+    public void clear() {
+        super.clear();
+        myAttributesSelected.setText("");        
+    }
+    
+    public void doAdditionalMouseBehavior(MouseEvent e) {
+        if (e.getSource().equals(myAddAttributeButton)) {
+            if (myAttributesBox.getSelectedItem().equals(NEW_TEXT)) {
+                String name = JOptionPane.showInputDialog("Enter the name of your new attribute");
+                String value = JOptionPane.showInputDialog("Enter the value for " + name);
+                myAttributesSelected.setText(myAttributesSelected.getText()
+                   + name + " " + value + "\n");
+            }
+            else {
+                myAttributesSelected.setText(myAttributesSelected.getText()
+                    + myAttributesBox.getSelectedItem().toString()
+                    + " " + myAttributeValue.getText() + "\n");
+            }
+        }
+        else if (e.getSource().equals(myDeleteAttributeButton)) {
+            myAttributesSelected.replaceSelection("");
+        }
+    }
+    
+    /**
+     * gets the attribute map of name to value.
+     * @return a map of string to string
+     */
+    public Map<String, String> getAttributes() {
+        Map<String, String> attributeMap = new HashMap<String, String>();
+        String fullAttributes = myAttributesSelected.getText();
+        while (!fullAttributes.equals("")) {
+            int index = fullAttributes.indexOf("\n");
+            String attribute = fullAttributes.substring(0, index);
+            String[] nameAndValue = attribute.split(" ");
+            if (nameAndValue.length != 1) {
+                attributeMap.put(nameAndValue[0], nameAndValue[1]);
+            }
+            else {
+                attributeMap.put(nameAndValue[0], "");
+            }
+            fullAttributes = fullAttributes.substring(index+1);
+        }
+        return attributeMap;
+    }
+    
+    /**
+     * helper method to make a map from the string from a text area.
+     * @param s is a string from a text area.
+     */
+    public Map<String, String> makeMap(String s) {
+        Map<String, String> map = new HashMap<String, String>();
+        String[] subParts = s.split("\n");
+        for (String subPart : subParts) {
+            if (!subPart.equals("")) {
+                String[] nameAndValue = subPart.split(" ");
+                map.put(nameAndValue[0], nameAndValue[1]);
+            }
+        }
+        return map;
+    }
+
+}
---
+++ src/vooga/towerdefense/gameeditor/gamemaker/SubEditorScreen.java
@@ -0,0 +1,55 @@
+package vooga.towerdefense.gameeditor.gamemaker;
+
+import java.awt.BorderLayout;
+import java.awt.event.MouseEvent;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public abstract class SubEditorScreen extends JPanel {
+    
+    /**
+     * constant for text area height.
+     */
+    public static final int TEXT_AREA_HEIGHT = 25;
+    /**
+     * constant for text area width.
+     */
+    public static final int TEXT_AREA_WIDTH = 10;
+    /**
+     * title for this screen.
+     */
+    private String myTitle;
+    /**
+     * box to enter the name of the game element.
+     */
+    private JTextField myNameBox;
+    
+    public SubEditorScreen(String title) {
+        myTitle = title;
+        addCharacteristicsPanel();
+    }
+    
+    private void addCharacteristicsPanel() {
+        JPanel characteristicsPanel = new JPanel();
+        myNameBox = new JTextField(TEXT_AREA_WIDTH);
+        characteristicsPanel.add(new JLabel("Name: "));
+        characteristicsPanel.add(myNameBox);
+        add(characteristicsPanel, BorderLayout.NORTH);
+    }
+    
+    public String getTitle() {
+        return myTitle;
+    }
+    
+    public String getName() {
+        return myNameBox.getText();
+    }
+    
+    public void clear() {
+        myNameBox.setText("");
+    }
+    
+    public abstract void doAdditionalMouseBehavior(MouseEvent e);
+
+}
