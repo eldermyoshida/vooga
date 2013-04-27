a06a9344acb738eb5eeafb19e07f83fcae2408ce
Henrique Moraes <hrm8@duke.edu>
2013-04-27 17:21:28 -0400

    JD LoggerReflection

--- src/util/logger/LoggerReflection.java
+++ src/util/logger/LoggerReflection.java
@@ -1,20 +1,42 @@
 package util.logger;
 
+/**
+ * Uses reflection to refer to the calling class of a method
+ * @author Henrique Moraes
+ *
+ */
 public class LoggerReflection {
+    private static int CALLER_INDEX = 4;
 
+    /**
+     * private constructor. should not be initialized
+     */
     private LoggerReflection () {
     }
 
+    /**
+     * 
+     * @return The class name of the caller
+     */
     public static String getCallerClassName () {
         return getElement().getClassName();
     }
 
+    /**
+     * 
+     * @return The method name from which the caller calls
+     */
     public static String getCallerMethodName () {
         return getElement().getMethodName();
     }
 
+    /**
+     * 
+     * @return The StackTrace Element associated with the calling
+     * method
+     */
     private static StackTraceElement getElement () {
         StackTraceElement[] element = Thread.currentThread().getStackTrace();
-        return element[4];
+        return element[CALLER_INDEX];
     }
 }
