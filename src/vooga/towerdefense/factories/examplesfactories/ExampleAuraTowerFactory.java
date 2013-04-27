d3189e1337a676c97635c5cde7b7ffcf9fd27668
Matthew Roy <matthewaroy@gmail.com>
2013-04-26 21:04:12 -0400

    Fixed up some problems with the example factories

--- src/vooga/towerdefense/factories/examplesfactories/ExampleAuraTowerFactory.java
+++ src/vooga/towerdefense/factories/examplesfactories/ExampleAuraTowerFactory.java
@@ -3,7 +3,6 @@ package vooga.towerdefense.factories.examplesfactories;
 import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
-
 import vooga.towerdefense.action.Action;
 import vooga.towerdefense.action.FindTargets;
 import vooga.towerdefense.action.actionlist.LaunchProjectile;
@@ -27,52 +26,61 @@ import util.Location;
  */
 public class ExampleAuraTowerFactory extends GameElementFactory {
 
-	GameMap myMap;
-	GameElementDefinition myDef;
+    GameMap myMap;
+    GameElementDefinition myDef;
 
-	/**
-	 * @param name
-	 * @param def
-	 */
-	public ExampleAuraTowerFactory (String name, TowerDefinition def) {
-		super(name, def);
-		myMap = getMap();
-		myDef = new TowerDefinition();
+    /**
+     * @param name
+     * @param def
+     */
+    public ExampleAuraTowerFactory (String name, TowerDefinition def) {
+        super(name, "Projectile", def);
+        myMap = getMap();
+        myDef = new TowerDefinition();
 
-	}
+    }
 
-	public GameElement createElement (Location putHere) {
-		AttributeManager AM = getDefaultAM();
+    public GameElement createElement (Location putHere) {
+        AttributeManager AM = new AttributeManager();
 
-		GameElement myTower;
-		if (putHere != null) {
-			myTower = new GameElement(myDef.getImage(), putHere,
-					new Dimension(50, 50), AM);
-		}
-		else {
-			myTower = new GameElement(myDef.getImage(),
-					myDef.getCenter(), myDef.getSize(), AM);
-		}
+        GameElement myTower;
+        if (putHere != null) {
+            myTower = new GameElement(myDef.getImage(), putHere,
+                                      new Dimension(50, 50), AM);
+        }
+        else {
+            myTower = new GameElement(myDef.getImage(),
+                                      myDef.getCenter(), myDef.getSize(), AM);
+        }
 
-		myTower.addActions(createActions(myTower));
-		return myTower;
-	}
+        myTower.addActions(createActions(myTower));
+        return myTower;
+    }
 
-	/**
-	 * Manually add game element actions (for testing only actions will be predefined in level editor)
-	 */
-	 @Override
-	 public List<Action> createActions(GameElement element) {
-		 ArrayList<Action> actions = new ArrayList<Action>();
-		 FindTargets findTargets =
-				 new FindTargets(myMap, element.getCenter(), 
-						 element.getAttributeManager().getAttribute(AttributeConstants.ATTACK_RADIUS));
-		 findTargets.addFollowUpAction(new LaunchProjectile(myMap, element.getCenter(), 
-				 new ExampleDosProjectileFactory("projectileFactory", new ProjectileDefinition())));
-		 /*new ModifyAttributeValue(AM
-                 .getAttribute(AttributeConstants.AURA_EFFECT), AttributeConstants.HEALTH));*/
-		 actions.add(findTargets);
-		 return actions;
-	 }
+    /**
+     * Manually add game element actions (for testing only actions will be predefined in level
+     * editor)
+     */
+    @Override
+    public List<Action> createActions (GameElement element) {
+        ArrayList<Action> actions = new ArrayList<Action>();
+        FindTargets findTargets =
+                new FindTargets(myMap, element.getCenter(),
+                                element.getAttributeManager()
+                                        .getAttribute(AttributeConstants.ATTACK_RADIUS));
+        findTargets
+                .addFollowUpAction(new LaunchProjectile(
+                                                        myMap,
+                                                        element.getCenter(),
+                                                        new ExampleDosProjectileFactory(
+                                                                                        "projectileFactory",
+                                                                                        new ProjectileDefinition())));
+        /*
+         * new ModifyAttributeValue(AM
+         * .getAttribute(AttributeConstants.AURA_EFFECT), AttributeConstants.HEALTH));
+         */
+        actions.add(findTargets);
+        return actions;
+    }
 
 }
