e798e56c7ba0206398089bfde4d206465715527a
Angelica <angeschwartz@gmail.com>
2013-04-27 22:52:41 -0400

    temporarily bypassing mapeditorscreen

--- src/vooga/towerdefense/gameeditor/gamemaker/ViewEditorScreen.java
+++ src/vooga/towerdefense/gameeditor/gamemaker/ViewEditorScreen.java
@@ -35,7 +35,8 @@ public class ViewEditorScreen extends GameEditorScreen {
     /**
      * next screen constant.
      */
-    private static final String NEXT_SCREEN_NAME = "mapeditor.MapEditorScreen";
+   // private static final String NEXT_SCREEN_NAME = "mapeditor.MapEditorScreen";
+    private static final String NEXT_SCREEN_NAME = "PlayerEditorScreen";
     /**
      * package name for the available game screens.
      */
