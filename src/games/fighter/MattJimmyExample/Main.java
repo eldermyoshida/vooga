8fbd0902f70b1d30ce1c9be7349139f8cc053427
James Wei <jcw46@duke.edu>
2013-04-28 16:51:01 -0400

    Added Main and SmashCloneRunAlone classes

---
+++ src/games/fighter/MattJimmyExample/Main.java
@@ -0,0 +1,18 @@
+package games.fighter.MattJimmyExample;
+
+import javax.swing.JFrame;
+
+/**
+ * Starts up the example game.
+ * 
+ * @author James Wei
+ *
+ */
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+        SmashCloneRunAlone controller = new SmashCloneRunAlone();
+        controller.run();
+    }
+    
+}
