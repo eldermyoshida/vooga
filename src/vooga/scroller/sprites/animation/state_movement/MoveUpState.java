182a1c184b1cd5487cfc871f01396a47ef80bdce
Scott Valentine <scott.douglas.valentine@gmail.com>
2013-04-28 00:46:25 -0400

    renamed movement states

--- src/vooga/scroller/sprites/animation/state_movement/MoveUpState.java
+++ src/vooga/scroller/sprites/animation/state_movement/MoveUpState.java
@@ -1,16 +1,16 @@
-package vooga.scroller.sprites.animation.movement;
+package vooga.scroller.sprites.animation.state_movement;
 
 import vooga.scroller.sprites.Sprite;
 import vooga.scroller.util.ISpriteView;
 
-public class MoveUp extends SpriteMovement {
+public class MoveUpState extends SpriteMovementState {
 
     public static final int STATE_ID = -4;
 
     
     private ISpriteView myStandView;
 
-    public MoveUp (Sprite sprite, ISpriteView move, ISpriteView stand, double speed) {
+    public MoveUpState (Sprite sprite, ISpriteView move, ISpriteView stand, double speed) {
         super(sprite, move, Sprite.UP_DIRECTION, speed);
         myStandView = stand;
     }
