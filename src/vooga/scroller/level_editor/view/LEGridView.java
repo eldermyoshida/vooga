3e47f70c297ef97498a297400100ce7061868fef
Dagbedji Fagnisse <dfagnisse@gmail.com>
2013-04-27 21:41:48 -0400

    checkstyled LEGridView

--- src/vooga/scroller/level_editor/view/LEGridView.java
+++ src/vooga/scroller/level_editor/view/LEGridView.java
@@ -26,78 +26,56 @@ import vooga.scroller.util.mvc.vcFramework.WindowComponent;
  * @author Danny Goodman, Deo Fagnisse
  */
 public class LEGridView extends WindowComponent<LevelEditing> 
-implements Scrollable, Renderer<LevelEditing>{
-    private class GridPositionListener implements MouseListener {
+                        implements Scrollable, Renderer<LevelEditing> {
 
-        @Override
-        public void mouseClicked (MouseEvent e) {
-            if (e.getButton() == 3) { // Right Click
-                deleteSprite(e.getX(), e.getY());
-            }
-            else if (e.getButton() == 1) { // Left Click
-                createSprite(e.getX(), e.getY());
-            }
-        }
-
-        @Override
-        public void mouseEntered (MouseEvent e) {
-        }
-
-        @Override
-        public void mouseExited (MouseEvent e) {
-        }
-
-        @Override
-        public void mousePressed (MouseEvent e) {
-        }
-
-        @Override
-        public void mouseReleased (MouseEvent e) {
-        }
-
-    }
-
-
-    private static LEGridView D;
-    
-    public static double getDefaultHeightRatio () {
-        return LevelEditing.VIEW_CONSTANTS.DEFAULT_GRIDVIEW_HEIGHT_RATIO;
-    }
-
-    public static double getDefaultWidthRatio () {
-        return LevelEditing.VIEW_CONSTANTS.DEFAULT_GRIDVIEW_WIDTH_RATIO;
-    }
 
     /**
      * 
      */
     private static final long serialVersionUID = 8266835201464623542L;
+    
     private LEGrid myGrid;
-
     private GridSpinner myGridSpinner;
 
+    
     /**
      * Specify a container parent and a width and height ratio.
      * 
-     * @param parent
-     * @param d
-     * @param e
+     * @param parent - container for this view instance
+     * @param r - main renderable for this view instance
      */
     public LEGridView (IView<LevelEditing> parent, Renderable<LevelEditing> r) {
-        super(parent,((LEGrid) r).getPixelSize());
+        super(parent, ((LEGrid) r).getPixelSize());
         this.addMouseListener(new GridPositionListener());
 
     }
 
+    /**
+     * Internal getter for Default Height Ratio
+     * @return - height ratio
+     */
+    private double getDefaultHeightRatio () {
+        return LevelEditing.VIEW_CONSTANTS.DEFAULT_GRIDVIEW_HEIGHT_RATIO;
+    }
+
+    /**
+     * Internal getter for Default width Ratio
+     * @return - width ratio
+     */
+    private double getDefaultWidthRatio () {
+        return LevelEditing.VIEW_CONSTANTS.DEFAULT_GRIDVIEW_WIDTH_RATIO;
+    }
+
+
     private void createSprite (int x, int y) {
         String cmd = CommandConstants.CREATE_SPRITE + CommandConstants.SPACE
-                     + x + CommandConstants.SPACE + y;
+                + x + CommandConstants.SPACE + y;
         process(cmd);
     }
 
     private void deleteSprite (int x, int y) {
         String cmd = CommandConstants.DELETE_SPRITE + CommandConstants.SPACE
-                     + x + CommandConstants.SPACE + y;
+                + x + CommandConstants.SPACE + y;
         process(cmd);
     }
 
@@ -107,6 +85,11 @@ implements Scrollable, Renderer<LevelEditing>{
         Dimension res = new Dimension((int) (d.width * getDefaultWidthRatio()),
                                       (int) (d.height * getDefaultHeightRatio()));
         return res;
+    }
+
+    @Override
+    public Renderable<LevelEditing> getRenderable () {
+        return myGrid;
     }
 
     @Override
@@ -138,6 +121,7 @@ implements Scrollable, Renderer<LevelEditing>{
         return myGrid.isValidForSimulation();
     }
 
+
     /**
      * Paint the contents of the canvas.
      * 
@@ -158,6 +142,19 @@ implements Scrollable, Renderer<LevelEditing>{
 
 
     @Override
+    public void render (Renderable<LevelEditing> r) {
+        if (r instanceof LEGrid) {
+            render((LEGrid) r);
+        }
+        else try {
+            throw new LevelEditorException("LEGridView cannot render" + r.getClass().getName());
+        }
+        catch (LevelEditorException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
     public void setRenderable (Renderable<LevelEditing>  r) {
         myGrid = (LEGrid) r;
         setSize(myGrid.getPixelSize());
@@ -165,46 +162,66 @@ implements Scrollable, Renderer<LevelEditing>{
     }
 
 
-    @Override
-    public void render (Renderable<LevelEditing> r) {
-       if (r instanceof LEGrid) {
-           render((LEGrid) r);
-       }
-    else try {
-        throw new LevelEditorException("LEGridView cannot render" + r.getClass().getName());
-    }
-    catch (LevelEditorException e) {
-        e.printStackTrace();
-    }
-    }
-
-    @Override
-    public Renderable<LevelEditing> getRenderable () {
-        return myGrid;
-    }
-
-
+    /**
+     * Launch a simulation of the active tab
+     */
     public void simulate () {
         myGrid.simulate();
     }
 
+    /**
+     * Update the size of the grid in the active tab
+     */
     public void updateGridSize () {
 
         myGridSpinner = new GridSpinner(myGrid.getWidthInBlocks(),
                                         myGrid.getHeightInBlocks());
         int a = (int) JOptionPane.showConfirmDialog(
-                                        null, myGridSpinner, 
-                                        "Update Grid Height and Width", 
-                                        JOptionPane.OK_CANCEL_OPTION);
-            if (a == 0) {
-              process(CommandConstants.CHANGE_GRID_SIZE + " " + 
-                      myGridSpinner.getGridWidth() + ", " +
-                      myGridSpinner.getGridHeight());
-            }
+                                                    null, myGridSpinner, 
+                                                    "Update Grid Height and Width", 
+                                                    JOptionPane.OK_CANCEL_OPTION);
+        if (a == 0) {
+            process(CommandConstants.CHANGE_GRID_SIZE + " " + 
+                    myGridSpinner.getGridWidth() + ", " +
+                    myGridSpinner.getGridHeight());
+        }
     }
-    
-    
-    
+
+
+
+    private class GridPositionListener implements MouseListener {
+        private static final int LEFT_CLICK = 3;
+        private static final int RIGHT_CLICK = 1;
+        
+        @Override
+        public void mouseClicked (MouseEvent e) {
+            if (e.getButton() == LEFT_CLICK) {
+                deleteSprite(e.getX(), e.getY());
+            }
+            else if (e.getButton() == RIGHT_CLICK) {
+                createSprite(e.getX(), e.getY());
+            }
+        }
+
+        @Override
+        public void mouseEntered (MouseEvent e) {
+        }
+
+        @Override
+        public void mouseExited (MouseEvent e) {
+        }
+
+        @Override
+        public void mousePressed (MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased (MouseEvent e) {
+        }
+
+    }
+
+
 
 
 
