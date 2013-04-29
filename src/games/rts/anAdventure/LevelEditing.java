148bc540ab16e9063f1df58374d072e6272bb2ef
Henrique Moraes <hrm8@duke.edu>
2013-04-29 03:24:09 -0400

    Added authors to example game

--- src/games/rts/anAdventure/LevelEditing.java
+++ src/games/rts/anAdventure/LevelEditing.java
@@ -4,6 +4,12 @@ import vooga.rts.leveleditor.gui.Canvas;
 import vooga.rts.resourcemanager.ImageLoader;
 import vooga.rts.resourcemanager.ResourceManager;
 
+/**
+ * Class used to invoke the level editor so the map xml 
+ * file could be generated
+ * @author Henrique Moraes & Sherry Liu
+ *
+ */
 public class LevelEditing {
 
     /**
