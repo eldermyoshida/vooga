e86ba892c111aa24883207d10024e198e52cfa97
Jimmy Longley <jimmylongley@gmail.com>
2013-04-29 18:47:07 -0400

    added a version of defend your castle to clowns

--- src/vooga/towerdefense/model/levels/Level.java
+++ src/vooga/towerdefense/model/levels/Level.java
@@ -7,20 +7,34 @@ import vooga.towerdefense.action.actionlist.LevelTimerAction;
 import vooga.towerdefense.model.GameModel;
 import vooga.towerdefense.model.rules.Rule;
 
+/**
+ * A basic level within the game.
+ * @author JLongley
+ *
+ */
 public class Level {
 
 	List<Action> myActions;
 	List<Rule> myRules;
-	
+
+	/**
+	 * The Level is a container for a list of actions and a list of rules that
+	 * apply when this level is active. For instance, a level could have an
+	 * action that spawns a wave of monsters, and a rule that gives the player
+	 * extra money for every kill during this level.
+	 * 
+	 * @param actions
+	 * @param rules
+	 */
 	public Level(List<Action> actions, List<Rule> rules) {
 		myActions = actions;
 		myRules = rules;
 	}
-	
+
 	public void start(GameModel model) {
 		model.addActions(myActions);
 	}
-	
+
 	public List<Rule> getRules() {
 		return myRules;
 	}
@@ -28,10 +42,10 @@ public class Level {
 	public List<Action> getActions() {
 		return myActions;
 	}
-	
+
 	public Integer getRemainingTime() {
-		for(Action action : myActions) {
-			if(action instanceof LevelTimerAction) {
+		for (Action action : myActions) {
+			if (action instanceof LevelTimerAction) {
 				return ((LevelTimerAction) action).getRemainingTime();
 			}
 		}
